ecfa50dcb1b717cfd588885b75b593a16feab83d
greg <greg>
1993-06-25 11:38:08 +0000

    Easy access to standalone executable miwriteimages.  Builds slice/frame lists as strings, writes to temp file, and runs miwriteimages via !.

--- matlab/general/miwriteimages.m
+++ matlab/general/miwriteimages.m
@@ -1,4 +1,5 @@
 function miwriteimages (filename, images, slices, frames)
+% MIWRITEIMAGES  write images to a MINC file
 %
 %   miwriteimages (filename, images, slices, frames)
 %
@@ -6,12 +7,16 @@ function miwriteimages (filename, images, slices, frames)
 %  file.  The MINC file must already exist and must have room for the 
 %  data.  slices and frames only tell miwriteimages where to put the data
 %  in the MINC file, they are not used to select certain columns from images.
+%
+%  Note that there is also a standalone executable miwriteimages; this 
+%  is called by miwriteimages.m via a shell escape.  Neither of these
+%  programs are meant for everyday use by the end user.
 
 slicelist = '';
 for i = 1:(length(slices) - 1)
-	slicelist = [slicelist int2str(slices(i)-1)];
+	slicelist = [slicelist int2str(slices(i)-1) ','];
 end
-slicelist = [slicelist int2str(slices(length(slices))-1)]
+slicelist = [slicelist int2str(slices(length(slices))-1)];
 
 if (nargin < 4)
 	framelist = '-'
@@ -19,9 +24,11 @@ else
 	if (~isempty(frames))
 		framelist = '';
 		for i = 1:(length(frames) - 1)
-			framelist = [framelist int2str(frames(i)-1)];
+			framelist = [framelist int2str(frames(i)-1) ','];
 		end
-		framelist = [framelist int2str(frames(length(frames))-1)]
+		framelist = [framelist int2str(frames(length(frames))-1)];
+	else
+		framelist = '-';
 	end
 end
 
@@ -33,4 +40,5 @@ end
 
 count = fwrite (outfile, images, 'double');
 
-unix (['miwriteimages ' filename ' ' slicelist ' ' framelist ' ' tempfile]);
+% disp(['miwriteimages ' filename ' ' slicelist ' ' framelist ' ' tempfile]);
+unix(['miwriteimages ' filename ' ' slicelist ' ' framelist ' ' tempfile]);
