70e86918070ef0e7b9f298e975c09fb690a4074c
wolforth <wolforth>
1993-06-25 11:44:46 +0000

    Initial revision

---
+++ matlab/general/Contents.m
@@ -0,0 +1,17 @@
+% PET image analysis
+%
+% MINC I/O functions
+%   openimage    - Open an image stored in a MINC file.
+%   newimage     - Create a new data set for image storage.
+%   getimages    - Get images from an open data set.
+%   putimages    - Put an image into a data set.
+%   getblooddata - Get blood data from a data set.
+%   closeimage   - Close an image data set.
+%
+% General utility functions
+%   calpix       - Generate a vector index for a point in
+%                  an image vector.
+%   viewimage    - View an image.
+%   spectral     - Generate the RGB numbers for a spectral
+%                  colourmap.
+%   getimageinfo - Get information about a data set.
