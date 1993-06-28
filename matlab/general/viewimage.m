884540b23d836c24753255efe51680e34acc8ab8
greg <greg>
1993-06-28 14:43:36 +0000

    Jettisoned pcolor and xv; everything now uses image.

--- matlab/general/viewimage.m
+++ matlab/general/viewimage.m
@@ -1,64 +1,41 @@
-function viewimage (image)
-% VIEWIMAGE (image) takes a vector or a square matrix
-%           containing an image.  On an SGI, it will
-%           display the image using Matlab's pcolor
-%           function.  On any other display, it saves
-%           the data in a temporary file, converts the
-%           data to a PGM, and then displays the
-%           result using XV.
+function viewimage (img)
+% VIEWIMAGE  displays a PET image from a vector or square matrix.
+%  viewimage (img) sets the colormap to spectral (the standard PET
+%  colormap) and uses MATLAB's image function to display the image.
+%  Works on either SGI's or Xterminals, with colours dithered
+%  to black and white on the Xterms.
+%
+%  Note that the colour scaling is currently a little overenthusiastic:
+%  any image, regardless of the magnitude of activity in it, will
+%  be scaled so its high points are white and its low points black.
 
-[x,y] = size (image);
-
+[x,y] = size (img);
 
 if (x==y)
     xsize = x;
 else
     xsize= x^.5;
+    if (xsize ~= floor (xsize))
+        error('Image must be square.');
+    end
     if (y ~= 1)
-        error('Image must be a vector or square.');
+        error('Image must be a vector if not square.');
     end
+    img = reshape (img, xsize, xsize);
 end
 
+% img is now square, xsize by xsize.  Set the colormap, and 
+% shift/scale img so that it maps onto 1..length(colormap)
 
-% Figure out if we have a colour machine or a monochrome machine
+colormap (spectral);
+num_colors = length (colormap);
 
-display = getenv('DISPLAY');
+lo = min(min(img));
+hi = max(max(img));
+img = ((img - lo) * ((num_colors-1) / (hi-lo))) + 1;
+disp (['Img min is ' int2str(min(min(img)))]);
+disp (['Img max is ' int2str(max(max(img)))]);
 
-if (strcmp(display,':0.0') | strcmp(display(1:4),'lear') | strcmp(display(1:4),'pria') | strcmp(display(1:4),'dunc') | strcmp(display(1:4),'port'))
-
-    pcolor (reshape(image,xsize,xsize)'); colormap (spectral); shading flat;
-
-else
-
-    % Assume a monochrome X-term
-    % Rotate the image and reshape it to a square
-
-    newimage = image - min(min(image));
-    newimagemax = max(max(newimage));
-    if (newimagemax == 0)
-       disp ('Warning: image is all zeros');
-    else
-        newimage = newimage .* (255/newimagemax);
-    end
-
-    eval(['newimage = reshape (newimage,' int2str(xsize) ',' int2str(xsize) ');' ]);
-    newimage = fliplr (newimage);
-    eval(['newimage = reshape (newimage,' int2str(x) ',' int2str(y) ');' ]);
-
-
-    % Find a unique file name
-
-    filename = tempfilename;
-
-
-    % Write the data to a temporary file
-
-    file_handle = fopen (filename, 'w');
-    fwrite (file_handle, newimage, 'char');
-    fclose (file_handle);
-
-    eval(['!rawtopgm ' int2str(xsize) ' ' int2str(xsize) ' ' filename ' | xv - &'])
-
-end
-
-
+% Now display it, and fix the y-axis to normal (rather than reverse) dir.
+image (img');
+set (gca, 'YDir', 'normal');
