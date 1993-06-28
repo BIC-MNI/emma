a77ee0d2401127285032893eed7083d68de5070a
wolforth <wolforth>
1993-06-28 00:38:41 +0000

    General bug fixing.  Still only generates K2 data.

--- matlab/rcbf/rcbf1.m
+++ matlab/rcbf/rcbf1.m
@@ -1,16 +1,26 @@
-% RCBF1  a first try at a two-compartment rCBF model (without V0)
-%        implemented as a MATLAB script with filename currently 
-%        hardcoded. (sorry)
+function rcbf1 (filename)
+% RCBF1  
+%        a first try at a two-compartment rCBF model (without V0
+%        or shifting) implemented as a MATLAB function.
+%
+%        rcbf1 (filename)
 
-cd /usr/people/wolforth/matlab
-img = openimage ('images/arnaud_20547.mnc');
+if (nargin ~= 1)
+    help rcbf1
+    error('Filename must be specified.');
+end
+
+img = openimage(filename);
 FrameTimes = getimageinfo (img, 'FrameTimes');
 FrameLengths = getimageinfo (img, 'FrameLengths');
 MidFrameTimes = FrameTimes + (FrameLengths / 2);
 PET = getimages (img, 1, 1:length(FrameTimes));
 
 disp ('RCBF1: calling findrl');
+
+rL = findrl (PET, MidFrameTimes, FrameLengths);
+[k2, rr] = findrr (img, MidFrameTimes, FrameLengths);
+
 keyboard
-rL = findrl (images, MidFrameTimes, FrameLengths);
 
-
+closeimage (img);
