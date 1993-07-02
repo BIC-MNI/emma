5b7d54dd41072803f537793c906ed99c553e11e6
greg <greg>
1993-07-02 11:05:55 +0000

    Made it return an error message rather than aborting via error()

--- matlab/general/check_sf.m
+++ matlab/general/check_sf.m
@@ -1,6 +1,7 @@
 function status = check_sf (handle, slices, frames)
 %  check_sf  - for internal use only
 %  status = 0: all is OK
+%  status = error message if there's anything wrong
 
 % check_sf - makes sure that the given slices and frames vectors
 % are consistent with the image specified by handle.  Checks for:
@@ -19,25 +20,25 @@ num_frames = eval(['NumFrames' int2str(handle)]);
 num_slices = eval(['NumSlices' int2str(handle)]);
 
 if (length(slices) > 1) & (length(frames) > 1)
-	error ('Cannot specify both multiple slices and multiple frames');
+   status = 'Cannot specify both multiple slices and multiple frames';
 end
 
 if (num_frames == 0)
-	if ~isempty (frames)
-		disp ('Warning: image has no frames, frame list will be ignored');
-	end
+   if ~isempty (frames)
+      disp ('Warning: image has no frames, frame list will be ignored');
+   end
 end
 
 if (isempty (frames)) & (num_frames > 0)
-	error ('Image has a time dimension; you must specify frames');
+   status = 'Image has a time dimension; you must specify frames';
 end
 
 if (num_slices == 0)
-	if ~isempty (slices)
-		disp ('Warning: image has no slices, slice list will be ignored');
-	end
+   if ~isempty (slices)
+      disp ('Warning: image has no slices, slice list will be ignored');
+   end
 end
 
 if (isempty (slices)) & (num_slices > 0)
-	error ('Image has a slice dimension; you must specify slices');
+   status = 'Image has a slice dimension; you must specify slices';
 end
