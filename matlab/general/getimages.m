4e9a92b641728d8cbf71e11f39fedecc761b440c
wolforth <wolforth>
1993-07-07 19:53:00 +0000

    Added comments, and memory use limiting.

--- matlab/general/getimages.m
+++ matlab/general/getimages.m
@@ -15,8 +15,8 @@ function images = getimages (handle, slices, frames)
 %    If the file is non-dynamic (ie. has no time dimension), then you
 %      can either not specify the frames vector, or make it an empty
 %      vector.  Either method will work.  If, however, you include a
-%	    list of frame numbers, they will be ignored and a warning message
-%		 printed.
+%      list of frame numbers, they will be ignored and a warning message
+%      printed.
 %
 %  The returned matrix, images, contains the images as vectors, one per
 %  column.  Thus if you read in 5 128x128 images, the matrix images
@@ -33,34 +33,34 @@ function images = getimages (handle, slices, frames)
 % ------------------------------ MNI Header ----------------------------------
 %@NAME       : getimages
 %@INPUT      : handle - tells which MINC file (or internal-to-MATLAB
-%					image data) to read from
-%					slices - list of slices (1-based) to read
-%					frames - list of frames (1-based) to read
+%              image data) to read from
+%              slices - list of slices (1-based) to read
+%              frames - list of frames (1-based) to read
 %@OUTPUT     : 
 %@RETURNS    : images - matrix whose columns contain entire images
-%					layed out linearly.
+%              layed out linearly.
 %@DESCRIPTION: Reads images from the MINC file associated with a MATLAB
-%					image handle.  The handle must have an associated MINC file;
-%					purely internal image sets are not yet supported.  
+%              image handle.  The handle must have an associated MINC file;
+%              purely internal image sets are not yet supported.  
 %
-% 					Note that if care is not taken, this can easily take up
-%					large amounts of memory.  Each image takes up 128 k of
-%					MATLAB memory, so reading all frames for a single slice
-%					from a 21-frame dynamic study will take up 2,688 k.
-%					When various analysis routines are carried out on this
-%					data, the amount of memory allocated by MATLAB can
-%					easily triple or quadruple.  getimages attempts to combat
-%					this by assigning a "maximum" number of images for each
-%					of PET's five SGI's (as of 93/7/6: priam, duncan, lear,
-%					portia, yorick) depending on their current memory 
-%					configurations.  If the number of slices/frames specified
-%					is greater than this "maximum", getimages will print a 
-%					warning and then read the data.  
+%              Note that if care is not taken, this can easily take up
+%              large amounts of memory.  Each image takes up 128 k of
+%              MATLAB memory, so reading all frames for a single slice
+%              from a 21-frame dynamic study will take up 2,688 k.
+%              When various analysis routines are carried out on this
+%              data, the amount of memory allocated by MATLAB can
+%              easily triple or quadruple.  getimages attempts to combat
+%              this by assigning a "maximum" number of images for each
+%              of PET's four main SGI's (as of 93/7/6: priam, duncan, lear,
+%              portia) depending on their current memory 
+%              configurations.  If the number of slices/frames specified
+%              is greater than this "maximum", getimages will print a 
+%              warning and then read the data.  
 %
 %@METHOD     : 
 %@GLOBALS    : Filename#
 %@CALLS      : check_sf to check validity of slices/frames arguments
-%					mireadimages (CMEX)
+%              mireadimages (CMEX)
 %@CREATED    : June 1993, Greg Ward & Mark Wolforth
 %@MODIFIED   : 6 July 1993, Greg Ward: 
 %-----------------------------------------------------------------------------
@@ -72,8 +72,8 @@ if (nargin < 2) | (nargin > 3)
    error ('Incorrect number of arguments.');
 end
 
-if (nargin < 3)			% no frames vector given, so make it empty
-	frames = [];
+if (nargin < 3)         % no frames vector given, so make it empty
+   frames = [];
 end
 
 % Make the handle's filename global, and check that it exists
@@ -105,13 +105,13 @@ if (strcmp (host, 'priam'))        % 80 MB of main memory on priam
    max_im = 80;                    % 80 images = 10 MB
 elseif (strcmp (host, 'duncan') | strcmp (host, 'lear'))
    max_im = 40;                    % 40 images = 5 MB
-elseif (strcmp (host, 'portia') | strcmp (host, 'yorick'))
-   max_im = 12;						  % 12 images = 1.5 MB
+elseif strcmp (host, 'portia')
+   max_im = 12;                    % 12 images = 1.5 MB
 else
    max_im = 30;                    % completely arbitrary for unknown machine
 end
 
-num_im = max (length (slices), length (frames))
+num_im = max (length (slices), length (frames));
 if num_im > max_im
    disp (['getimages warning: reading enough images to possibly bring ' host ' to its knees']);
 end
