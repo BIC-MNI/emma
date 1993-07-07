fa8997e0ef1d4c17f4e639abb52cfd8e52fcd434
wolforth <wolforth>
1993-07-07 16:05:43 +0000

    Allowed turning the colourbar off, and returned handles for all graphics objects created.

--- matlab/general/viewimage.m
+++ matlab/general/viewimage.m
@@ -1,13 +1,20 @@
-function viewimage (img)
+function [fig_handle, image_handle, bar_handle] = viewimage (img, colourbar)
 % VIEWIMAGE  displays a PET image from a vector or square matrix.
-%  viewimage (img) sets the colormap to spectral (the standard PET
-%  colormap) and uses MATLAB's image function to display the image.
+%  viewimage (img) sets the colourmap to spectral (the standard PET
+%  colourmap) and uses MATLAB's image function to display the image.
 %  Works on either SGI's or Xterminals, with colours dithered
 %  to black and white on the Xterms.
 %
 %  Note that the colour scaling is currently a little overenthusiastic:
 %  any image, regardless of the magnitude of activity in it, will
 %  be scaled so its high points are white and its low points black.
+%
+%  viewimage (img, colourbar flag) turns the colourbar on or off.
+%  The default is on, but by specifying colourbar flag = 0, the
+%  colourbar will be turned off.
+%
+%  [fig_handle, image_handle, bar_handle] = viewimage (img, colourbar)
+%
 
 [x,y] = size (img);
 
@@ -24,15 +31,18 @@ else
     img = reshape (img, xsize, xsize);
 end
 
-% img is now square, xsize by xsize.  Set the colormap, and 
-% shift/scale img so that it maps onto 1..length(colormap)
+if (nargin==1)
+  colourbar = 1;
+end
 
-display = getenv('DISPLAY');
 
-if (strcmp(display,':0.0') | strcmp(display(1:4),'lear') | strcmp(display(1:4),'pria') | strcmp(display(1:4),'dunc') | strcmp(display(1:4),'port'))
+% img is now square, xsize by xsize.  Set the colourmap, and 
+% shift/scale img so that it maps onto 1..length(colourmap)
+
+if (strcmp (get (0, 'BlackAndWhite'), 'off'))
    colormap (spectral);
 else
-   colormap (gray);
+   colormap (gray .^ 1.5);           % darken it a little
 end
 num_colors = length (colormap);
 
@@ -42,25 +52,29 @@ img = ((img - lo) * ((num_colors-1) / (hi-lo))) + 1;
 
 % Now display it, and fix the y-axis to normal (rather than reverse) dir.
 
-subplot(1,2,1);
+fig_handle = gcf;
+
+image_handle = subplot(1,2,1);
 image (img');
 axis('xy','square');
 
 % For some reason, Matlab didn't want to change the position
 % of the figure at this point, so I moved that to the end.....
 
-% Draw a colorbar beside the image
+% Draw a colourbar beside the image
 
-subplot(1,2,2);
-image((1:num_colors)');
-axis('xy');
-set(gca,'Xticklabels', []);
-set(gca,'Ytick',0:8:64);
-lab = linspace(lo, hi, 9);
-for i=1:9, labels = str2mat(labels,num2str(lab(i))); end;
-labels(1,:)=[];
-set(gca,'Yticklabels', labels);
-set(gca,'Position', [.85, .1, .1, .8]);
+if (colourbar)
+  bar_handle = subplot(1,2,2);
+  image((1:num_colors)');
+  axis('xy');
+  set(gca,'Xticklabels', []);
+  set(gca,'Ytick',0:8:64);
+  lab = linspace(lo, hi, 9);
+  for i=1:9, labels = str2mat(labels,num2str(lab(i))); end;
+  labels(1,:)=[];
+  set(gca,'Yticklabels', labels);
+  set(gca,'Position', [.85, .1, .1, .8]);
+end
 
 % Make the main image a reasonable size
 
