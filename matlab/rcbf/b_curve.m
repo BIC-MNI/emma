3bfe7e7ae00e9e69d808d709e0882c6a82d0fff1
greg <greg>
1993-07-14 09:50:38 +0000

    Straigtened out and optimised.  Still does the (totally bogus) four- parameter fit, though.

--- matlab/rcbf/b_curve.m
+++ matlab/rcbf/b_curve.m
@@ -2,40 +2,44 @@ function integral = b_curve (args)
 
 global g_even ts_even A flengths ftimes midftimes
 
-expthing = exp(-args(2)*ts_even);	% ts_even for the convolution
-
 % N.B.: alpha = args(1)
 %        beta = args(2)
 %       gamma = args(3)
 %       delta = args(4)
 
-% This gets g(t - delta) from g(t)
+% This gets the shifted activity function, g(t - delta), by shifting
+% g(t) to the right (ie. subtract delta from its actual times, ts_even)
+% and resampling at the "correct" times ts_even.
 
 shifted_g_even = lookup ((ts_even-args(4)), g_even, ts_even);
 
-c = conv(shifted_g_even,expthing);
+% Now calculate exp (-beta * t) in the ts_even time domain, and perform
+% the convolution with the *shifted* activity g(t-delta).
 
+expthing = exp(-args(2)*ts_even); 	% ts_even for the convolution
+c = conv(shifted_g_even,expthing);
+c = c (1:length(ts_even));		% chop off points outside of the time
+					% domain we're interested in
+
+% Calculate the two main terms of the expression, alpha * (convoluted
+% stuff) and gamma * (shifted activity), and sum them.
+   
 i1 = args(1)*c;				% alpha * (convolution)
 i2 = args(3)*shifted_g_even;		% gamma * g(t - delta)
 
-i1 = i1(1:length(i2));			% chop off excess convoluted points
-i = i1+i2;				% all are now length of g_even
+i = i1+i2;
 
-i = lookup (ts_even,i,midftimes);
-% i = i (ts_even < 60);			% chop off times > 60 sec to match A
+% Note that we don't have to explicitly integrate here, because we
+% multiply by flengths to integrate, and then divide by flengths to
+% normalise.
 
-% ts_interval = ts_even(2) - ts_even(1); % assumes ts_even is evenly spaced!
-integral = zeros (size (A));
+integral = lookup (ts_even,i,midftimes);
+integral = integral (1:length(A));
 
-%for frm=1:length(A)
-%   in_range = (ts_even >= ftimes(frm)) & (ts_even <= ftimes(frm+1));
-%   integral (frm) = trapz (ts_even(find(in_range)), i(find(in_range)));
-%   integral(frm) = sum (i (find(in_range))) * ts_interval;
-%   integral (frm) = i (frm) * flengths (frm);
-%end
 
-integral = i .* flengths;
-integral = integral(1:length(A)) ./ flengths(1:length(A));
+
+
+
 
 
 
