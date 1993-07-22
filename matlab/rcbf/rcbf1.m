f8e8903852d402b9f988df40e32175557f5e1e04
greg <greg>
1993-07-22 10:06:41 +0000

    Neatened up, standardised variable names

--- matlab/rcbf/rcbf1.m
+++ matlab/rcbf/rcbf1.m
@@ -1,9 +1,9 @@
-function [k1,k2] = rcbf1 (filename, slice, progress)
+function [K1,k2] = rcbf1 (filename, slice, progress)
 % RCBF1  
 %        a first try at a two-compartment rCBF model (without V0
 %        or shifting) implemented as a MATLAB function.
 %
-%        [k1,k2] = rcbf1 (filename, slice)
+%        [K1,k2] = rcbf1 (filename, slice)
 
 % Input argument checking
 
@@ -23,14 +23,14 @@ end
 
 if (progress); disp ('Reading image information'); end
 img = openimage(filename);
-FrameTimes = getimageinfo (img, 'FrameTimes');
-FrameLengths = getimageinfo (img, 'FrameLengths');
-MidFrameTimes = FrameTimes + (FrameLengths / 2);
-PET = getimages (img, slice, 1:length(FrameTimes));
+fstarts = getimageinfo (img, 'fstarts');
+flengths = getimageinfo (img, 'flengths');
+midftimes = fstarts + (flengths / 2);
+PET = getimages (img, slice, 1:length(fstarts));
 PET = PET .* (PET > 0);			% set all negative values to zero
 
 if (progress); disp ('Calculating rL image'); end
-rL = findrl (PET, MidFrameTimes, FrameLengths);
+rL = findrl (PET, midftimes, flengths);
 
 % Find the minimum/maximum of "reasonable" points in rL (i.e. those within
 % one standard deviation of the mean -- this may seem overly picky, but
@@ -46,36 +46,32 @@ if (progress); disp ('Calculating k2/rR lookup table'); end
 [Ca_even, ts_even] = resampleblood (img, 'even');
 
 k2_lookup = (0.001:0.02:5) / 60;
-[conv_int1, conv_int2] = findconvints (k2_lookup, ...
-	MidFrameTimes, FrameLengths, Ca_even, ts_even);
+[conv_int1, conv_int2] = findintconvo (Ca_even, ts_even, k2_lookup,...
+                            midftimes, flengths, [], midftimes);
 rR = conv_int1 ./ conv_int2;
 
-% Generate K1 and K2 images
+% Generate K1 and k2 images
 
 if (progress); disp ('Calculating k2 image'); end
 k2 = lookup(rR, k2_lookup, rL);
 
-if (progress); disp ('Calculating k1 image'); end
-int_activity = PET * FrameLengths;
-
+if (progress); disp ('Calculating K1 image'); end
+int_activity = PET * flengths;
 k2_conv_ints = lookup (k2_lookup, conv_int1, k2);
-k1 = int_activity ./ k2_conv_ints;
+K1 = int_activity ./ k2_conv_ints;
 
-nuke = find (isnan (k1));
-k1 (nuke) = zeros (size (nuke));
-nuke = find (isinf (k1));
-k1 (nuke) = zeros (size (nuke));
-
+nuke = find (isnan (K1) | isinf K1));
+K1 (nuke) = zeros (size (nuke));
 
 % Magic number time: convert the values to the correct units.
-% k2 is currently expressed in units of s^-1 but should be expressed
-% min^-1; k1 is in (nCi * sec * (g blood)) / ((mL tissue) * counts * sec)
+% k2 is currently expressed in units of 1/sec but should be expressed
+% 1/min; K1 is in (nCi * sec * (g blood)) / ((mL tissue) * counts * sec)
 % and should be (mL blood) / ((g tissue) * minute).  Since blood and
 % tissue are both taken to be 1.05 g / mL, and 1 nCi = 37 count/sec, and
 % of course 1 min = 60 sec, the conversion factor is 2013.605442176871
 
 k2 = k2 * 60;
-k1 = k1 * 2013.605442176871;
+K1 = K1 * 2013.605442176871;
 
 % Cleanup
 
