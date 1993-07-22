a9085aa23a5bc4ffb67917c2280e1c8913d3b55d
greg <greg>
1993-07-22 10:59:29 +0000

    Improved documentation

--- matlab/general/getmask.m
+++ matlab/general/getmask.m
@@ -1,21 +1,28 @@
 function mask = getmask (image)
 % GETMASK returns a mask that is the same size as the passed image.
 %         The mask consists of 0's and 1's, and is created interactively
-%         by the user.
+%         by the user.  Currently, a threshold algorithm is used, 
+%         based on the input argument image: the user selects a 
+%         threshold using a slider (the default starting value is 
+%         1.8), and getmask selects all points in image greater than
+%         the mean value of the entire image multiplied by threshold 
+%         the threshold.  It then displays image as masked by
+%         that threshold value, so the user can refine the threshold
+%         to his/her satisfaction.
 
 if (nargin ~= 1)
    help getmask;
    error ('Incorrect number of input arguments.');
 end
 
-img = image;
+img = image;                            % copy because it is made global
 mu = mean(mean(img)); 			% in case it happens to be square
 threshold = 1.8; 			% initial value (from Hiroto)
 temp = img > threshold * mu; 		% "binary" temp - all 1's and 0's
 [fh, iah] = viewimage (temp .* img); 	% returns handles: figure, image axes
 
 pos = get (iah, 'Position');
-pos (2) = pos(2) + 1.05*pos (4);	% err, assum<ing normalised units
+pos (2) = pos(2) + 1.05*pos (4);	% assuming normalised units
 pos (4) = .05;
 
 tobj = text('units', 'normal', 'position', [.5 1.15], 'string', num2str(threshold));
