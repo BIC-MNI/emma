f4df5925b197e32116d3e5645b30ca9a9d7f7765
wolforth <wolforth>
1993-07-26 19:51:26 +0000

    Fixed a major problem with the old version: it didn't work.  Consider this version as the initial version.

--- matlab/general/smooth.m
+++ matlab/general/smooth.m
@@ -6,14 +6,44 @@ function new_data = smooth (old_data)
 %
 %
 
-mask = [0 0 1 0 0 ...
-        0 1 1 1 0 ...
-	1 1 1 1 1 ...
-	0 1 1 1 0 ...
-	0 0 1 0 0 ];
 
-new_data = zeros(length(old_data));
-    
-for pos = 1:length(old_data-26)
-    new_data (pos+13) = mean(mask .* old_data (pos:pos+25));
+if (nargin ~= 1)
+   help smooth
+   error ('Incorrect number of input arguments.');
 end
+
+
+mask = [0 0 1 0 0; ...
+        0 1 1 1 0; ...
+	1 1 1 1 1; ...
+	0 1 1 1 0; ...
+	0 0 1 0 0  ];
+
+[x,y] = size (old_data);
+
+if (x==y)
+    xsize = x;
+else
+    xsize= x^.5;
+    if (xsize ~= floor (xsize))
+        error('Image must be square.');
+    end
+    if (y ~= 1)
+        error('Image must be a vector if not square.');
+    end
+    old_data = reshape (old_data, xsize, xsize);
+end
+
+%
+%  Now old_data is a square image matrix
+%
+
+new_data = zeros(xsize, xsize);
+
+for i=3:xsize-2
+    for j=3:xsize-2
+        new_data(i,j) = mean (mean (old_data(i-2:i+2, j-2:j+2) .* mask));
+    end
+end
+
+new_data = reshape (new_data, xsize^2, 1);
