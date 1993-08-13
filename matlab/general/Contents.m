afdf4ee00adef833f4fe473eeab81dc1e999fb5a
greg <greg>
1993-08-13 16:38:19 +0000

    *** empty log message ***

--- matlab/general/Contents.m
+++ matlab/general/Contents.m
@@ -3,15 +3,14 @@
 % MINC I/O functions
 %   openimage    - Open an image stored in a MINC file.
 %   newimage     - Create a new data set for image storage.
+%   getimageinfo - Get information about a data set.
 %   getimages    - Get images from an open data set.
 %   putimages    - Put an image into a data set.
+%   getimglines  - Get a contiguous range of horizontal lines from an image.
 %   getblooddata - Get blood data from a data set.
 %   closeimage   - Close an image data set.
 %
 % General utility functions
-%   calpix       - Generate a vector index for a point in
-%                  an image vector.
+%   calpix       - Generate a vector index for a point in an image vector.
 %   viewimage    - View an image.
-%   spectral     - Generate the RGB numbers for a spectral
-%                  colourmap.
-%   getimageinfo - Get information about a data set.
+%   spectral     - Generate the RGB numbers for a spectral colourmap.
