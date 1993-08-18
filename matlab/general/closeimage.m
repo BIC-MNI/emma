e35f3c7aa426dba6f05e5acad7b063b371ba7e38
greg <greg>
1993-08-18 13:50:52 +0000

    Updated to handle the new set of global variables created by openimage   or newimage (Filename#, DimSizes#, FrameTimes#, FrameLengths#). Added provision for closing multiple images at once.

--- matlab/general/closeimage.m
+++ matlab/general/closeimage.m
@@ -1,12 +1,9 @@
-function closeimage (handle)
+function closeimage (handles)
 
-eval(['clear global Filename'     int2str(handle)]);
-eval(['clear global NumFrames',   int2str(handle)]);
-eval(['clear global NumSlices',   int2str(handle)]);
-eval(['clear global ImageSize',   int2str(handle)]);
-eval(['clear global PETimages'    int2str(handle)]);
-eval(['clear global FrameTimes'   int2str(handle)]);
-eval(['clear global FrameLengths',int2str(handle)]);
-eval(['clear global AvailFrames', int2str(handle)]);
-eval(['clear global AvailSlices', int2str(handle)]);
-eval(['clear global CurLine',     int2str(handle)]);
+for handle = handles
+   eval(['clear global Filename'     int2str(handle)]);
+   eval(['clear global DimSizes',    int2str(handle)]);
+   eval(['clear global FrameTimes'   int2str(handle)]);
+   eval(['clear global FrameLengths',int2str(handle)]);
+end
+   
