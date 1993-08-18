1f4f88aca4b58d6599bc4d22d08f5ea1f8fd6c1d
greg <greg>
1993-08-18 11:12:15 +0000

    Added a few comments, and made it handle slices exactly the same   as frames (ie. they can be omitted).

--- matlab/general/miwriteimages.m
+++ matlab/general/miwriteimages.m
@@ -8,37 +8,66 @@ function miwriteimages (filename, images, slices, frames)
 %  data.  slices and frames only tell miwriteimages where to put the data
 %  in the MINC file, they are not used to select certain columns from images.
 %
+%  Also, the slices and frames must be valid and consistent with the MINC
+%  file, which must exist and have an image variable in it.  The number
+%  of images to write (implied by the number of elements in slices or frames)
+%  must be the same as the number of columns in the matrix images.  Since
+%  miwriteimages only expects to be called by putimages, none of these
+%  requirements are checked here -- all that is done by putimages.
+%
 %  Note that there is also a standalone executable miwriteimages; this 
 %  is called by miwriteimages.m via a shell escape.  Neither of these
 %  programs are meant for everyday use by the end user.
 
-slicelist = '';
-for i = 1:(length(slices) - 1)
-	slicelist = [slicelist int2str(slices(i)-1) ','];
+% If the slices vector was supplied and is non-empty, then convert it
+% to a string (eg. [1 2 3] becomes '1,2,3') for passing to the executable
+% miwriteimages.  If the vector was not supplied or is empty, then 
+% make the string simply a '-' to indicate no slices.  (This is only valid
+% for a slice-less file.)
+
+if (nargin < 3)
+   slicelist = '-';
+else
+   if (~isempty (slices))
+      slicelist = '';
+      for i = 1:(length(slices) - 1)
+         slicelist = [slicelist int2str(slices(i)-1) ','];
+      end
+      slicelist = [slicelist int2str(slices(length(slices))-1)];
+   else
+      slicelist = '-';
+   end
 end
-slicelist = [slicelist int2str(slices(length(slices))-1)];
+
+% Now do the exact same thing for frames.
 
 if (nargin < 4)
-	framelist = '-'
+   framelist = '-'
 else
-	if (~isempty(frames))
-		framelist = '';
-		for i = 1:(length(frames) - 1)
-			framelist = [framelist int2str(frames(i)-1) ','];
-		end
-		framelist = [framelist int2str(frames(length(frames))-1)];
-	else
-		framelist = '-';
-	end
+   if (~isempty(frames))
+      framelist = '';
+      for i = 1:(length(frames) - 1)
+         framelist = [framelist int2str(frames(i)-1) ','];
+      end
+      framelist = [framelist int2str(frames(length(frames))-1)];
+   else
+      framelist = '-';
+   end
 end
+
+% Generate a temporary filename, create the file, and write the entire
+% images matrix to it as doubles.
 
 tempfile = tempfilename;
 outfile = fopen (tempfile, 'w');
 if (outfile == -1)
-	error (['Could not open temporary file ' tempfile ' for writing!']);
+   error (['Could not open temporary file ' tempfile ' for writing!']);
 end
 
 count = fwrite (outfile, images, 'double');
+
+% Finally, do a shell escape to miwriteimages to write the data from the
+% temporary (raw) file to the MINC file.
 
 % disp(['miwriteimages ' filename ' ' slicelist ' ' framelist ' ' tempfile]);
 unix(['miwriteimages ' filename ' ' slicelist ' ' framelist ' ' tempfile]);
