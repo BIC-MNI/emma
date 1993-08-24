e7aca6cf958be52bfc3d3d871d99b63bd25b6cba
greg <greg>
1993-08-24 11:59:30 +0000

    *** empty log message ***

--- matlab/general/Contents.m
+++ matlab/general/Contents.m
@@ -1,15 +1,29 @@
 % PET image analysis
 %
-% MINC I/O functions
+% High-level MINC I/O functions
 %   openimage     - Open an image stored in a MINC file.
-%   newimage      - Create a new data set for image storage.
-%   getimageinfo  - Get information about a data set.
-%   getimages     - Get images from an open data set.
-%   putimages     - Put an image into a data set.
+%   newimage      - Create a new MINC file.
+%   getimageinfo  - Get information about an opened MINC file.
+%   getimages     - Get images from an open MINC file.
+%   putimages     - Put an image into a MINC file created with newimage.
 %   getimglines   - Get a contiguous range of horizontal lines from an image.
 %   getblooddata  - Get blood data from a data set.
+%   getmask       - Interactively calculate a threshold mask.
 %   resampleblood - Get resampled blood data from a data set.
 %   closeimage    - Close an image data set.
+%
+% Low-level (CMEX or standalone executables) MINC I/O functions
+%
+%   mireadimages  - Read images from a MINC file (used by getimages).
+%   mireadvar     - Read a hyperslab from any NetCDF variable.
+%   micreate      - Create a new MINC file from scratch.
+%   micreateimage - Create the MIimage variable in a new MINC file.
+%   miwriteimages - Write images to a MINC file (used by putimages).
+%   miwritevar    - Write a hyperslab to any NetCDF variable.
+%   miinquire     - Get NetCDF variable, dimension, or attribute information.
+%
+%     Note: these seven functions should not generally be called by 
+%     general purpose image analysis applications.
 %
 % General utility functions
 %   calpix        - Generate a vector index for a point in an image vector.
@@ -18,7 +32,7 @@
 %   C_trapz       - Fast CMEX function for trapezoidal integration.
 %   lookup        - Fast CMEX function for linear interpolation.
 %   nframeint     - Fast CMEX integration across frames.
-%   deriv         - Calculate the derivative of a function
+%   deriv         - Calculate the derivative of a numerical function
 %   smooth        - Perform a simple spatial smoothing on an image.
 %
 % rCBF analysis functions
