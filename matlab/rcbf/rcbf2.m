9a73a2d6bc4d74e7e21c9278c463e31cb774edc9
greg <greg>
1993-08-25 15:04:34 +0000

    Updated the documentation to refer only to our "RCBF Analysis" document;    we still need to finish this document to get all the equation numbers    right.

--- matlab/rcbf/rcbf2.m
+++ matlab/rcbf/rcbf2.m
@@ -3,31 +3,40 @@ function [K1,k2,V0,delta] = rcbf2 (filename, slice, progress, correction)
 % RCBF2 a two-compartment (triple-weighted integral) rCBF model.
 %
 %       [K1,k2,V0,delta] = rcbf2 (filename, slice)
-%
-% rcbf2 implements the three-weighted integral method of calculating k2, K1,
-% and V0 (in that order) for a particular slice.  This function also returns
-% the delay value calculated for blood correction.  It first reads in a
-% great mess of data (viz., the brain activity for every frame of the slice,
-% frame start times and lengths, blood sample activity, and blood sample
-% times).  Then, a simple mask is created and used to filter out roughly all
-% points outside the head.
-%
-% The actual calculations follow the procedure outlined in Ohta
-% et. al.  (see the references section in the document "RCBF Analysis Using
-% Matlab").  Thus, all equation numbers cited in this M-file refer to that
-% paper.  
 % 
-% Starting with Eq. 23, we calculate the ratio on the left-hand
-% side, rL, for every pixel.  Then, we generate lookup table for the
-% right-hand ratio, rL, for a series of k2 values.  The calculated rL values
-% are used to lookup in this table (since rL == rR) to find k2 for every
-% pixel.  Then, Eq. 24 is used to calculate K1.  This requires calculating
-% the moderately complicated int_activity (left hand side of Eq. 24) and the
-% extremely complicated k2_conv_ints (right hand side).  However, the
-% expression for k2_conv_ints appeared already in the numerator of rR, so we
-% preserve that lookup table as conv_int1 and use it to lookup k2_conv_ints.
-% These two long vectors (with one number for every pixel) are then divided
-% to get K1.  Finally, V0 is calculated via Eq. 26.
+% rcbf2 implements the three-weighted integral method of calculating
+% k2, K1, and V0 (in that order) for a particular slice.  This
+% function also returns the delay value calculated for blood
+% correction.  It first reads in a great mess of data (viz., the brain
+% activity for every frame of the slice, frame start times and
+% lengths, plasma activity, and blood sample times).  Then, a simple
+% mask is created and used to filter out roughly all points outside
+% the head.
+% 
+% The actual calculations follow the procedure outlined in the
+% document "RCBF Analysis Using Matlab".  Occasionally, comments in
+% the source code or documentation for various functions involved in
+% the analysis will refer to equations in this document.  The most
+% relevant functions in this respect are rcbf2 itself, correctblood
+% and findintconvos.
+% 
+% The starting point of the three-weighted integration method is Eq.
+% 10 of the RCBF document.  The left hand side of this equation, rL,
+% is calculated for every pixel.  Then, a lookup table relating a
+% series of k2 values to the rR (the right-hand side of Eq. 10) is
+% calculated.  This lookup table should have a few hundred elements,
+% as calculating rR is considerably more expensive than calculating
+% rL.  Since rL and rR are equal, we use the pixel-wise values of rL
+% to lookup k2 for every pixel.
+% 
+% Then, Eq. XXX is used to calculate K1.  This requires calculating
+% the moderately complicated int_activity (left hand side of Eq. XXX)
+% and the extremely complicated k2_conv_ints (right hand side).
+% However, the expression for k2_conv_ints appeared already in the
+% numerator of rR, so we preserve that lookup table as conv_int1 and
+% use it to lookup k2_conv_ints.  These two long vectors (with one
+% number for every pixel) are then divided to get K1.  Finally, V0 is
+% calculated via Eq. YYY.
 
 
 % Input argument checking
