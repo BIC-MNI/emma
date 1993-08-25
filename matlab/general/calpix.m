9b1b30a41e6625da148f3df2ed9e3c42bd12bdd3
wolforth <wolforth>
1993-08-25 18:13:20 +0000

    changed the help info slightly

--- matlab/general/calpix.m
+++ matlab/general/calpix.m
@@ -1,9 +1,13 @@
 function cp = calpix(x,y)
-%cp = CALPIX(x,y) generates the vector index (1 to 16384) of 
-%	the x and y coordinates of a pixel in a 128*128 image.
-%	It uses	the simple formula 
+% CALPIX  generates the vector index of a point
 %
-%	          cp = round(x) + 128 * round(y-1)
+%       cp = calpix(x,y)
+%
+% generates the vector index (1 to 16384) of 
+% the x and y coordinates of a pixel in a 128*128 image.
+% It uses the simple formula 
+%
+%       cp = round(x) + 128 * round(y-1)
 %
 
 cp = round(x) + 128 * round(y-1);
