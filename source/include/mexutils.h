8d2b7b956af7ee757faec5dcbbf7e2ae2d890fe6
wolforth <wolforth>
1993-10-04 14:27:01 +0000

    Initial revision

---
+++ source/include/mexutils.h
@@ -0,0 +1,23 @@
+#ifndef _MEX_UTILS_H
+#define _MEX_UTILS_H
+
+#ifndef _GPW_H
+#include "gpw.h"
+#endif
+
+/* 
+ * Error codes returned by functions in this library
+ */
+
+#define mexARGS_TOO_BIG -1
+#define mexARGS_INVALID -2
+
+/*
+ * Function prototypes.  Details in mexutils.c
+ */
+
+int   ParseOptions   (Matrix *OptVector, int MaxOptions, Boolean *debug);
+char *ParseStringArg (Matrix *Mstr, char *Cstr []);
+int   ParseIntArg    (Matrix *Mvector, int MaxSize, long Cvector[]);
+
+#endif
