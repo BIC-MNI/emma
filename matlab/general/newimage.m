8efc9020eeb8a76941f3486cdaeb1a679789932e
greg <greg>
1993-10-06 14:39:04 +0000

    Uses recent additions to miinquire to inherit image type,   valid range, and orientation from the parent file. A few minor fixes to the argument-juggling code.

--- matlab/general/newimage.m
+++ matlab/general/newimage.m
@@ -1,9 +1,9 @@
 function handle = newimage (NewFile, DimSizes, ParentFile, ...
-                            ImageType, ValidRange, DimOrder)
+                            ImageType, ValidRange, Orientation)
 % NEWIMAGE  create a new MINC file, possibly descended from an old one
 %
 %  handle = newimage (NewFile, DimSizes, ParentFile, ...
-%                     ImageType, ValidRange, DimOrder)
+%                     ImageType, ValidRange, Orientation)
 %
 % creates a new MINC file.  NewFile and DimSizes must always be given,
 % although the number of elements required in DimSizes varies
@@ -32,7 +32,7 @@ function handle = newimage (NewFile, DimSizes, ParentFile, ...
 %                to the height/width of the parent MINC file.  In no
 %                case can the height or width be zero -- these two
 %                dimensions must always exist in a MINC file.  See
-%                below, under "DimOrder", for details on how slices,
+%                below, under "Orientation", for details on how slices,
 %                width, and height are mapped to MIzspace, MIyspace,
 %                and MIxspace for the various conventional image
 %                viewpoints.
@@ -54,10 +54,10 @@ function handle = newimage (NewFile, DimSizes, ParentFile, ...
 %                32767] for short, etc.  NOTE: this parameter is currently
 %                ignored.
 %
-%   DimOrder   - a string describing the orientation of the images,
-%                one of 'transverse' (the default), 'sagittal', or
+%   Orientation  - a string describing the orientation of the images,
+%                one of 'transverse', 'sagittal', or
 %                'coronal'.  Transverse images are the default if
-%                DimOrder is not supplied.  Recall that in the MINC
+%                Orientation is not supplied.  Recall that in the MINC
 %                standard, zspace, yspace, and xspace all have
 %                definite meanings with respect to the patient: z
 %                increases from inferior to superior, x from left to
@@ -90,6 +90,9 @@ function handle = newimage (NewFile, DimSizes, ParentFile, ...
 %@CREATED    : June 1993, Greg Ward & Mark Wolforth
 %@MODIFIED   : 6-17 Aug 1993 - totally overhauled (GPW).
 %              18 Aug 1993   - fixed up argument parsing code
+%              30-31 Sep 1993- uses new miinquire options to inherit
+%                              image type/valid range/orientation;
+%                              a few more fixes to the argument handling code
 %-----------------------------------------------------------------------------
 
 
@@ -105,17 +108,23 @@ end
 
 % If at least the parent file was given, let's open it so we can override
 % the defaults on the other arguments with values from the parent file.
+% Note also that if we open the file, we will read in the type of the
+% image variable.  This makes determining the type and valid range
+% of the new file a little neater.
 
 if (nargin >= 3) 
    if (~isempty (ParentFile))
       Parent = openimage (ParentFile);
+      ParentType = miinquire (ParentFile, 'vartype', 'image');
    else
       ParentFile = '-';           % indicates that no parent file opened
       Parent = -1;                % so does this 
+      ParentType = '';
    end
 else
    ParentFile = '-';           % indicates that no parent file opened
    Parent = -1;                % so does this 
+   ParentType = '';
 end
 
 
@@ -128,46 +137,74 @@ if (nargin <= 6)
    % Do nothing -- all arguments are supplied
 end
 
-if (nargin <= 5)                  % DimOrder not supplied, set to default
-   DimOrder = 'transverse';
+if (nargin <= 5)                  % Orientation not supplied, set to blank
+   Orientation = '';
 end
 
 if (nargin <= 4)                  % ValidRange not supplied either
-   ValidRange = [];                   % will be set after we know the type
+   ValidRange = [];               % will be set after we know the type
 end
 
 if (nargin <= 3)                  % ImageType not supplied
-   ImageType = 'byte';
+   ImageType = '';
 end
 
-% Now go through all the optional arguments (except ParentFile) and if 
-% they were empty, give them defaults.  Again, this should let ParentFile
-% override the defaults.
+% Now go through all the optional arguments (except ParentFile) and if
+% they were empty (ie. not found in the argument list, and set empty
+% by the if (nargin...) statements above), give them defaults.  Note 
+% that if the parent file was given, we use miinquire to get the 
+% default value from it, rather than using the "default defaults".
 
 if (isempty (ImageType))
-   ImageType = 'byte';
-end
-
-% The default valid ranges here are taken from /usr/include/limits.h
-
-if (isempty (ValidRange))
-   if (strcmp (ImageType, 'byte'))
-      ValidRange = [0 255];
-   elseif (strcmp (ImageType, 'short'))
-      ValidRange = [-32768 32767];
-   elseif (strcmp (ImageType, 'long'))
-      ValidRange = [-2147483648 2147483647];
-   elseif (strcmp (ImageType, 'float'))
-      ValidRange = [1.17549435e-38 3.40282347e+38];
-   elseif (strcmp (ImageType, 'double'))
-      ValidRange = [2.2250738585072014e-308 1.7976931348623157e+308];
-   else
-      error (['Invalid image type: ' ImageType]);
+   if (Parent == -1)              % no parent file? use the default default
+      ImageType = 'byte';
+   else                           % else get the type and valid range 
+      ImageType = ParentType;
    end
 end
 
-if (isempty (DimOrder))
-   DimOrder = 'transverse';
+
+% Determine the valid range for the new file.  Note that in this case,
+% we do not check just that a parent file was given; we also make sure
+% that ImageType is the same as the type of the image in the parent
+% file.  Thus, if the user does not supply a type, the new file will
+% inherit both type and valid range from its parent.  If the user
+% supplies a type that is the same as the type in the parent file, the
+% valid range is inherited.  If, however the user supplies a
+% *different* type from what is in the parent file, then the valid
+% range will simply be the default for that type.  (Of course these
+% will ALL be overridden if ValidRange is already set, ie. if the user
+% supplied one in the arguments to newimage.)
+
+if (isempty (ValidRange))
+   if (Parent ~= -1) & (strcmp (ImageType, ParentType))
+      ValidRange = miinquire (ParentFile, 'attvalue', 'image', 'valid_range');
+   else
+
+   % The default valid ranges here are taken from /usr/include/limits.h
+
+      if (strcmp (ImageType, 'byte'))
+         ValidRange = [0 255];
+      elseif (strcmp (ImageType, 'short'))
+         ValidRange = [-32768 32767];
+      elseif (strcmp (ImageType, 'long'))
+         ValidRange = [-2147483648 2147483647];
+      elseif (strcmp (ImageType, 'float'))
+         ValidRange = [1.17549435e-38 3.40282347e+38];
+      elseif (strcmp (ImageType, 'double'))
+         ValidRange = [2.2250738585072014e-308 1.7976931348623157e+308];
+      else
+         error (['Invalid image type: ' ImageType]);
+      end
+   end
+end
+
+if (isempty (Orientation))
+   if (Parent == -1)	          % no parent file, so use default default
+      Orientation = 'transverse';
+   else
+      Orientation = miinquire (ParentFile, 'orientation');
+   end
 end
 
 s = size(DimSizes);
@@ -176,7 +213,7 @@ if (min(s) ~= 1) | ( (max(s) ~= 2) & (max(s) ~= 4) )
 end
 
 if (max (s) == 2)
-   if (isempty (ParentFile)) | (Parent == -1)
+   if (Parent == -1)
       error ('Must supply all 4 dimension sizes if parent file is not given');
    else
       DimSizes (3) = getimageinfo(Parent,'ImageHeight');
@@ -186,18 +223,18 @@ end
 
 % At this point, we have a four-element DimSizes, and if ParentFile was
 % given then Parent contains the handle to the open MINC file.  Also, if
-% ParentFile was not given, ImageType, ValidRange, and DimOrder are
+% ParentFile was not given, ImageType, ValidRange, and Orientation are
 % set.  So let's create the new MINC file, copying the patient, study
 % and acquisition variables if possible.
 
-%disp (['New file: ' NewFile]);
-%disp (['Old file: ' ParentFile]);    % will be set to '' if none given by caller
-%disp ('DimSizes: ');
-%disp (DimSizes);
-%disp (['Image type: ' ImageType]);
-%disp ('Valid range:');
-%disp (ValidRange);
-%disp (['Orientation: ', DimOrder]);
+disp (['New file: ' NewFile]);
+disp (['Old file: ' ParentFile]);    % will be set to '' if none given by caller
+disp ('DimSizes: ');
+disp (DimSizes);
+disp (['Image type: ' ImageType]);
+disp ('Valid range:');
+disp (ValidRange);
+disp (['Orientation: ', Orientation]);
 
 execstr = sprintf ('micreate %s %s patient study acquisition', ...
                    ParentFile, NewFile);
@@ -207,11 +244,11 @@ if (result ~= 0)
 end
 
 % Now create the image variable in the new MINC file, using the current
-% values of DimSizes and DimOrder.  Note that the sprintf implicitly
+% values of DimSizes and Orientation.  Note that the sprintf implicitly
 % assumes that DimSizes has exactly four elements!
 
 execstr = sprintf ('micreateimage %s %d %d %d %d %s', ...
-                   NewFile, DimSizes, DimOrder);
+                   NewFile, DimSizes, Orientation);
 [result,output] = unix (execstr);
 if (result ~= 0)
    error (['Error running ' execstr ' to create image in file ' NewFile]);
