a8eba182ffe19bc6c3a883b254de5e9e5e90cd6b
wolforth <wolforth>
1993-10-18 13:52:03 +0000

    Made some major revisions: 1) Fixed the memory problems by careful tweaking and inclusion    of new CMEX functions. 2) Fixed a sneaky little bug that caused the ts_even variable to    get shorter on successive slices.  This caused a crash in later    slices of a study. 3) Added the ability to handle multiple slices. 4) Added a batch mode that turns off interactive mask generation.

--- matlab/rcbf/rcbf2.m
+++ matlab/rcbf/rcbf2.m
@@ -1,8 +1,9 @@
-function [K1,k2,V0,delta] = rcbf2 (filename, slice, progress, correction)
+function [K1,k2,V0,delta] = rcbf2_slice (filename, slices, progress, ...
+                                         correction, batch)
 
 % RCBF2 a two-compartment (triple-weighted integral) rCBF model.
 %
-%       [K1,k2,V0,delta] = rcbf2 (filename, slice)
+%       [K1,k2,V0,delta] = rcbf2 (filename, slices)
 % 
 % rcbf2 implements the three-weighted integral method of calculating
 % k2, K1, and V0 (in that order) for a particular slice.  This
@@ -49,141 +50,191 @@ elseif (nargin < 3)
    correction = 1;
 elseif (nargin < 4)
    correction = 1;
-elseif (nargin > 4)
+elseif (nargin < 5)
+   batch = 1;
+elseif (nargin > 5)
    help rcbf2
    error('Incorrect number of arguments.');
 end
 
-if (length(slice)~=1)
-   help rcbf2
-   error('<Slice> must be a scalar.');
-end
+total_slices = length(slices);
+K1 = zeros(16384,total_slices);
+k2 = zeros(16384,total_slices);
+V0 = zeros(16384,total_slices);
+delta = zeros(1,total_slices);
 
-% Input arguments are checked, so now we can do some REAL work, ie.
-% read in all the data we need.  FrameTimes, FrameLengths, and 
-% MidFTimes should be self-explanatory.  Ca_even is the blood
-% activity resampled at some evenly-spaced time domain; ts_even
-% is the actual points of that domain.  Ca_mft is the blood activity
-% integrated frame-by-frame (ie. the average of Ca over every frame).
-
-if (progress); disp ('Reading image information and generating mask'); end
 img = openimage(filename);
 FrameTimes = getimageinfo (img, 'FrameTimes');
 FrameLengths = getimageinfo (img, 'FrameLengths');
 MidFTimes = FrameTimes + (FrameLengths / 2);
-[g_even, ts_even] = resampleblood (img, 'even');
+[g_even, orig_ts_even] = resampleblood (img, 'even');
 
+XCAL = 0.11;
+% Apply the cross-calibration factor.
+rescale(g_even, (XCAL*37/1.05));        % units are decay / (g_tissue * sec)
 
-PET = getimages (img, slice, 1:length(FrameTimes));
-PET = PET * 37 / 1.05;                  % convert to decay / (g_tissue * sec)
-
-% Create the weighting functions (N.B. w1 is just one, so don't bother),
-% and calculate the three weighted integrals of PET.
+w1 = ones(length(MidFTimes), 1);
 w2 = MidFTimes;
 w3 = sqrt (MidFTimes);
 
-ImLen = size(PET,1);
-PET_int1 = ntrapz (MidFTimes, PET')';
-PET_int2 = ntrapz (MidFTimes, PET' .* (w2 * ones(1,ImLen)))';
-PET_int3 = ntrapz (MidFTimes, PET' .* (w3 * ones(1,ImLen)))';
-
-% Now use PET_int1 to create a simple mask, and mask all three PET integrals.
-% This does a good job of removing the outside-of-head data for CBF studies.
-
-mask = PET_int1 > mean(PET_int1);
-PET_int1 = PET_int1 .* mask;
-PET_int2 = PET_int2 .* mask;
-PET_int3 = PET_int3 .* mask;
-
-
-% Use getmask to interactively create a threshold mask, and then perform
-% delay/dispersion correction.
-
-% Apply the cross-calibration factor.
-XCAL = 0.11;
-g_even = g_even*XCAL*37/1.05;    % units are decay / (g_tissue * sec)
-
-if (correction)
-   mask = getmask (PET_int1);
-   A = (mean (PET (find(mask),:)))';
-   [ts_even, Ca_even, delta] = correctblood ...
-                       (A, FrameTimes, FrameLengths, g_even, ts_even, progress);
-else
-   Ca_even = g_even;
-end
-
-
-% Pick values of k2 and then calculate rR for every one.  (Ie. create
-% the lookup table).  This is done by first calculating various
-% weighted integrals (conv_int{1,2,3} and Ca_int{1,2,3}) which are
-% in turn used to calculate lots of other RCBF parameters.  Here 
-% we use them specifically to find rR.  NB. conv_int{1,2,3} are tables,
-% with one value for every k2 in k2_lookup.  However, Ca_int{1,2,3} are
-% simply scalars.
-
-if (progress); disp ('Generating k2/rR lookup table'); end
 k2_lookup = (-10:0.05:10) / 60;
-[conv_int1,conv_int2,conv_int3] = findintconvo (Ca_even,ts_even,k2_lookup,...
-      MidFTimes, FrameLengths, 1, w2, w3);
 
 
-Ca_mft = nframeint (ts_even, Ca_even, FrameTimes, FrameLengths);      
+for current_slice = 1:total_slices
+  % Input arguments are checked, so now we can do some REAL work, ie.
+  % read in all the data we need.  FrameTimes, FrameLengths, and 
+  % MidFTimes should be self-explanatory.  Ca_even is the blood
+  % activity resampled at some evenly-spaced time domain; ts_even
+  % is the actual points of that domain.  Ca_mft is the blood activity
+  % integrated frame-by-frame (ie. the average of Ca over every frame).
+  
+  if (progress)
+    disp (['Doing slice ', int2str(slices(current_slice))]);
+  end
 
-Ca_int1 = ntrapz(MidFTimes, Ca_mft);
-Ca_int2 = ntrapz(MidFTimes, (w2 .* Ca_mft));
-Ca_int3 = ntrapz(MidFTimes, (w3 .* Ca_mft));
+  if (progress)
+    disp ('Reading image information and generating mask');
+  end
+  
+  ts_even = orig_ts_even;
+  
+  PET = getimages (img, slices(current_slice), 1:length(FrameTimes), PET);
+  rescale (PET, (37/1.05));             % convert to decay / (g_tissue * sec)
 
-% Find the value of rL for every pixel of the slice.
-rL = ((Ca_int3 .* PET_int1) - (Ca_int1 .* PET_int3)) ./ ...
-     ((Ca_int3 .* PET_int2) - (Ca_int2 .* PET_int3));
+  % Create the weighting functions (N.B. w1 is just one, so don't bother),
+  % and calculate the three weighted integrals of PET.
+  
 
-rR = ((Ca_int3 * conv_int1) - (Ca_int1 * conv_int3)) ./ ...
+  if (progress)
+    disp ('Creating the weighted integrals.');
+  end
+
+  PET_int1 = ntrapz (MidFTimes, PET, w1);
+  PET_int2 = ntrapz (MidFTimes, PET, w2);
+  PET_int3 = ntrapz (MidFTimes, PET, w3);
+
+  
+  % Now use PET_int1 to create a simple mask, and mask all three PET integrals.
+  % This does a good job of removing the outside-of-head data for CBF studies.
+
+  if (progress)
+    disp ('Masking the weighted integrals.');
+  end
+
+  mask = PET_int1 > mean(PET_int1);
+  PET_int1 = PET_int1 .* mask;
+  PET_int2 = PET_int2 .* mask;
+  PET_int3 = PET_int3 .* mask;
+  clear mask;
+  
+  % Use getmask to interactively create a threshold mask, and then perform
+  % delay/dispersion correction.
+  
+  if (progress)
+    disp ('Performing delay correction.');
+  end
+
+  if (correction)
+    
+    % Since this is intended for batch mode operation,
+    % we will fix the mask with a threshold of 1.8
+
+    if (batch)
+      mask = PET_int1 > (1.8*mean(PET_int1));
+    else
+      mask = getmask (PET_int1);
+    end
+       
+    A = (mean (PET (find(mask),:)))';
+    clear mask;
+
+    [ts_even, Ca_even, delta(:,current_slice)] = correctblood ...
+        (A, FrameTimes, FrameLengths, g_even, ts_even, progress);
+  else
+    Ca_even = g_even;
+  end
+
+  
+  % Pick values of k2 and then calculate rR for every one.  (Ie. create
+  % the lookup table).  This is done by first calculating various
+  % weighted integrals (conv_int{1,2,3} and Ca_int{1,2,3}) which are
+  % in turn used to calculate lots of other RCBF parameters.  Here 
+  % we use them specifically to find rR.  NB. conv_int{1,2,3} are tables,
+  % with one value for every k2 in k2_lookup.  However, Ca_int{1,2,3} are
+  % simply scalars.
+  
+  
+  if (progress); disp ('Generating k2/rR lookup table'); end
+  [conv_int1,conv_int2,conv_int3] = findintconvo (Ca_even,ts_even,k2_lookup,...
+      MidFTimes, FrameLengths, w1, w2, w3);
+
+  Ca_mft = nframeint (ts_even, Ca_even, FrameTimes, FrameLengths);      
+
+  Ca_int1 = ntrapz(MidFTimes, (w1 .* Ca_mft));
+  Ca_int2 = ntrapz(MidFTimes, (w2 .* Ca_mft));
+  Ca_int3 = ntrapz(MidFTimes, (w3 .* Ca_mft));
+  
+  % Find the value of rL for every pixel of the slice.
+  rL = ((Ca_int3 .* PET_int1) - (Ca_int1 .* PET_int3)) ./ ...
+      ((Ca_int3 .* PET_int2) - (Ca_int2 .* PET_int3));
+  
+  rR = ((Ca_int3 * conv_int1) - (Ca_int1 * conv_int3)) ./ ...
       ((Ca_int3 * conv_int2) - (Ca_int2 * conv_int3));
 
-% Now, we must have the k2/rR lookup table in order by rR; however, 
-% we also want to keep k2_lookup in the original order.  This
-% is because the first lookup uses rL as a lookup into rR to
-% find k2 (which requires that rR be monotonic, ie. sorted) whereas
-% subsequent lookups all use k2 to find conv_int{1,2,3} -- which 
-% requires that k2_lookup be monotonic.  So k2_lookup will be the
-% list of k2's in order, and k2_sorted will be the same list, but 
-% in order according to the sorted rR.
+  % Now, we must have the k2/rR lookup table in order by rR; however, 
+  % we also want to keep k2_lookup in the original order.  This
+  % is because the first lookup uses rL as a lookup into rR to
+  % find k2 (which requires that rR be monotonic, ie. sorted) whereas
+  % subsequent lookups all use k2 to find conv_int{1,2,3} -- which 
+  % requires that k2_lookup be monotonic.  So k2_lookup will be the
+  % list of k2's in order, and k2_sorted will be the same list, but 
+  % in order according to the sorted rR.
+  
+  [rR,sort_order] = sort (rR);
+  k2_sorted = k2_lookup (sort_order);
 
-[rR,sort_order] = sort (rR);
-k2_sorted = k2_lookup (sort_order);
+  % Generate K1 and K2 images
+  
+  %=========================================================
+  if (progress); disp ('Calculating k2 image (via table lookup)'); end
+  k2(:,current_slice) = lookup(rR, k2_sorted, rL);
+  
+  
+  %=========================================================
+  if (progress); disp ('Calculating K1 image'); end
+  
+  % Note that PET_int1 = PET integrated across frames, with weighting
+  % function w1 = ones.
 
-% Generate K1 and K2 images
+  K1_numer = ((Ca_int3*PET_int1) - (Ca_int1 * PET_int3));
+  K1_denom = (Ca_int3 * lookup(k2_lookup,conv_int1,k2(:,current_slice))) - ...
+      (Ca_int1 * lookup(k2_lookup,conv_int3,k2(:,current_slice)));
+  K1(:,current_slice) = K1_numer ./ K1_denom;
+  
+  %=========================================================
+  if (progress); disp ('Calculating V0 image'); end
+  
+  % Now calculate V0, using Eq. 26.  Note that the second term of the
+  % numerator is just K1 .* k2_conv_ints; however, K1 was calculate 
+  % from int_activity ./ k2_conv_ints.  Therefore just use int_activity
+  % for that term.  Also, the first term of the numerator -- M(t) 
+  % integrated across frames -- was calculated above for masking
+  % the slice.  The denominator is just Ca(t) integrated across
+  % frames, so we need to use the resampled Ca(t) -- Ca_mft --
+  % for the integral.
+  
+  V0(:,current_slice) = (PET_int1 - (K1(:,current_slice) .* lookup(k2_lookup,conv_int1,k2(:,current_slice)))) / Ca_int1;
 
-%=========================================================
-if (progress); disp ('Calculating k2 image (via table lookup)'); end
-k2 = lookup(rR, k2_sorted, rL);
 
-
-%=========================================================
-if (progress); disp ('Calculating K1 image'); end
-
-% Note that PET_int1 = PET integrated across frames, with weighting
-% function w1 = ones.
-
-K1_numer = ((Ca_int3*PET_int1) - (Ca_int1 * PET_int3));
-K1_denom = (Ca_int3 * lookup(k2_lookup,conv_int1,k2)) - ...
-           (Ca_int1 * lookup(k2_lookup,conv_int3,k2));
-K1 = K1_numer ./ K1_denom;
-
-%=========================================================
-if (progress); disp ('Calculating V0 image'); end
-
-% Now calculate V0, using Eq. 26.  Note that the second term of the
-% numerator is just K1 .* k2_conv_ints; however, K1 was calculate 
-% from int_activity ./ k2_conv_ints.  Therefore just use int_activity
-% for that term.  Also, the first term of the numerator -- M(t) 
-% integrated across frames -- was calculated above for masking
-% the slice.  The denominator is just Ca(t) integrated across
-% frames, so we need to use the resampled Ca(t) -- Ca_mft --
-% for the integral.
-
-V0 = (PET_int1 - (K1 .* lookup(k2_lookup,conv_int1,k2))) / Ca_int1;
-
+  clear PET_int1 PET_int2 PET_int3
+  clear k2_sorted sort_order
+  clear conv_int1 conv_int2 conv_int3
+  clear Ca_int1 Ca_int2 Ca_int3 Ca_mft
+  clear K1_numer K1_denom
+  clear rL rR
+  clear ts_even Ca_even
+  
+end
 
 nuke = find (isnan (K1));
 K1 (nuke) = zeros (size (nuke));
@@ -194,12 +245,8 @@ nuke = find (isnan (V0));
 V0 (nuke) = zeros (size (nuke));
 nuke = find (isinf (V0));
 V0 (nuke) = zeros (size (nuke));
+  
 
 % Cleanup
 
 closeimage (img);
-
-
-
-
-
