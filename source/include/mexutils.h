b87806a34c1479c869b70fbee853630f6e796bdf
wolforth <wolforth>
1994-05-25 21:21:03 +0000

    Removed stuff that is in emmageneral.h emmageneral.h is now included automatically if not already included.

--- source/include/mexutils.h
+++ source/include/mexutils.h
@@ -1,18 +1,10 @@
 #ifndef _MEX_UTILS_H
 #define _MEX_UTILS_H
 
-/*
- * define a few useful constants and macros
- */
+#ifndef _EMMAGENERAL
+#include <emmageneral.h>
+#endif
 
-typedef unsigned char Boolean;
-
-#define TRUE 1
-#define FALSE 0
-
-#define min(A, B) ((A) < (B) ? (A) : (B))
-#define max(A, B) ((A) > (B) ? (A) : (B))
-#define abs(A)    ((A) < 0 ? ((A)*(-1)) : (A))
 
 /* 
  * Error codes returned by functions in this library
