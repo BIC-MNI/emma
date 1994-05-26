58eb10c77768f987a43a38a0b9b35c64ca7ae9cd
wolforth <wolforth>
1994-05-26 21:42:17 +0000

    included string.h

--- source/libsource/mincutil.c
+++ source/libsource/mincutil.c
@@ -19,6 +19,7 @@
 ---------------------------------------------------------------------------- */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "minc.h"
 #include "emmageneral.h"
 #include "mincutil.h"
