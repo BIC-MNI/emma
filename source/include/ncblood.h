dd2afa13a6f28d6472a796bdaea0a61cbf1c8e50
greg <greg>
1994-05-30 18:14:27 +0000

    Initial revision

---
+++ source/include/ncblood.h
@@ -0,0 +1,37 @@
+/****************************/
+/* Blood Data Root Variable */
+
+#define MIbloodroot         "blood_analysis"
+
+
+/*********************************/
+/* Standard Blood Data Variables */
+
+#define MIsamplestart       "sample_start"
+#define MIsamplestop        "sample_stop"
+#define MIsamplelength      "sample_length"
+#define MIcountstart        "count_start"
+#define MIcountlength       "count_length"
+#define MIcounts            "counts"
+#define MIemptyweight       "empty_weight"
+#define MIfullweight        "full_weight"
+#define MIcorrectedactivity "corrected_activity"
+#define MIactivity          "activity"
+
+
+/**********************************/
+/* Standard Blood Data Attributes */
+
+
+#define MIbloodfirstname    "first_name"
+#define MIbloodlastname     "last_name"
+#define MIbloodname         "patient_name"
+#define MIbloodrunnumber    "run_number"
+#define MIbloodstarttime    "start_time"
+#define MIblooddate         "date"
+#define MIbloodisotope      "isotope"
+#define MIbloodstudytype    "study_type"
+#define MIbloodbackground   "background"
+#define MIbloodcomplete     "complete"
+
+
