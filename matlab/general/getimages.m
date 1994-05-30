d6f6fcb3ad9411d426b0551edc02c5655bdc9ea5
greg <greg>
1994-05-30 15:59:51 +0000

    Completely rewrote the help section. Added support for partial image reading (start_row, num_rows)

--- matlab/general/getimages.m
+++ matlab/general/getimages.m
@@ -1,31 +1,50 @@
-function images = getimages (handle, slices, frames, old_matrix)
+function images = getimages (handle, slices, frames, old_matrix, start_row, num_rows)
 %GETIMAGES  Retrieve whole images from an open MINC file.
 %
-%  images = getimages (handle, slices[, frames])
+%  images = getimages (handle [, slices [, frames [, old_matrix ...
+%                      [, start_row [, num_rows]]]]])
 %
-%  reads the slices or frames
-%  listed in the vectors slices and frames from the MINC file specfied by
-%  handle.  
+%  reads whole or partial images from the MINC file specified by
+%  handle.  Either slices or frames can be a vector (to specify a
+%  set of several images), but at least one of them must be a scalar
+%  -- it is not possible to read images from both different slices and
+%  different frames at the same time.  (Multiple calls to getimages
+%  will be needed for this.)  If the file is non-dynamic (no time
+%  dimension), then the frames argument can be omitted or empty;
+%  likewise, if there is no slice dimension, the slices argument can
+%  be omitted or empty.  (But note that slices must be given if any
+%  frames are to be specified -- thus, it may be necessary to supply
+%  an empty matrix for slices in the unusual case of a MINC file with
+%  frames but no slice variation.)
 %
-%  Rules:
+%  The default behaviour of getimages is to read whole images and
+%  return them as MATLAB column vectors with the image rows stored
+%  sequentially.  If multiple images are read, then they will be
+%  returned as the columns of a matrix.  For instance, if 10 128x128
+%  images are read, then getimages will return a 16384x10 matrix; to
+%  extract a single image, use MATLAB's colon operator, as in foo
+%  (:,1) to extract all rows of column 1 of the matrix foo.
 %
-%    All slice and frame numbers are one-based.  
+%  To read partial images, you can specify a starting image row in
+%  start_row; if num_rows is not supplied and start_row is, then a
+%  single row is read.  
 %
-%    Only one of slices or frames can have multiple elements; that is,
-%      you cannot read in multiple slices and multiple frames at the
-%      same time.
+%  To try to conserve memory use, you can "recycle" MATLAB matrices
+%  when sequentially calling getimages to read in identically-sized
+%  blocks of image data.  This is done by simply passing your image
+%  matrix to getimages as old_matrix, eg:
 %
-%    If the file is non-dynamic (ie. has no time dimension), then you
-%      can either not specify the frames vector, or make it an empty
-%      vector.  Either method will work.  If, however, you include a
-%      list of frame numbers, they will be ignored and a warning message
-%      printed.
+%  img = [];
+%  for slice = 1:numslices
+%     img = getimages (handle, slice, 1:numframes, img);
+%     (process img)
+%  end
 %
-%  The returned matrix, images, contains the images as vectors, one per
-%  column.  Thus if you read in 5 128x128 images, the matrix images
-%  will have 16384 (=128*128) rows and 5.
+%  This will get around MATLAB's tendency to unnecessarily allocate
+%  new blocks of memory and leave old blocks unused.
 %
 %  EXAMPLES (assuming handle = openimage ('some_minc_file');)
+%
 %   To read in the first frame of the first slice:
 %     one_image = getimages (handle, 1, 1);
 %   To read in the first 10 frames of the first slice:
@@ -39,6 +58,9 @@ function images = getimages (handle, slices, frames, old_matrix)
 %              image data) to read from
 %              slices - list of slices (1-based) to read
 %              frames - list of frames (1-based) to read
+%              old_matrix - previously used block of memory to be recycled
+%              start_row - image row to start reading at
+%              num_rows - number of rows to read
 %@OUTPUT     : 
 %@RETURNS    : images - matrix whose columns contain entire images
 %              layed out linearly.
@@ -66,12 +88,14 @@ function images = getimages (handle, slices, frames, old_matrix)
 %              mireadimages (CMEX)
 %@CREATED    : June 1993, Greg Ward & Mark Wolforth
 %@MODIFIED   : 6 July 1993, Greg Ward: 
+%             30 May 1994, Greg Ward: added start_row and num_rows, 
+%                          completely rewrote help section
 %-----------------------------------------------------------------------------
 
 
 % Check for valid number of arguments
 
-if (nargin < 1) | (nargin > 4)
+if (nargin < 1) | (nargin > 6)
    error ('Incorrect number of arguments.');
 end
 
@@ -108,27 +132,33 @@ if ~isempty (s); error (s); end;
 % Find out the machine we're on so we can make some educated guesses
 % as to how much memory usage should elicit a warning
 
-host = getenv ('HOST');
-if (strcmp (host, 'priam'))        % 80 MB of main memory on priam
-   max_im = 80;                    % 80 images = 10 MB
-elseif (strcmp (host, 'duncan') | strcmp (host, 'lear'))
-   max_im = 40;                    % 40 images = 5 MB
-elseif strcmp (host, 'portia')
-   max_im = 12;                    % 12 images = 1.5 MB
-else
-   max_im = 30;                    % completely arbitrary for unknown machine
-end
+%host = getenv ('HOST');
+%if (strcmp (host, 'priam'))        % 80 MB of main memory on priam
+%   max_im = 80;                    % 80 images = 10 MB
+%elseif (strcmp (host, 'duncan') | strcmp (host, 'lear'))
+%   max_im = 40;                    % 40 images = 5 MB
+%elseif strcmp (host, 'portia')
+%   max_im = 12;                    % 12 images = 1.5 MB
+%else
+%   max_im = 30;                    % completely arbitrary for unknown machine
+%end
 
-num_im = max (length (slices), length (frames));
-if num_im > max_im
-   disp (['getimages warning: reading enough images to possibly bring ' host ' to its knees']);
-end
+%num_im = max (length (slices), length (frames));
+%if num_im > max_im
+%   disp (['getimages warning: reading enough images to possibly bring ' host ' to its knees']);
+%end
 
 % Now read the images!  (remembering to make slices and frames zero-based for
 % mireadimages).
 
 if (nargin < 4)
     images = mireadimages (filename, slices-1, frames-1);
-else
+elseif (nargin < 5)
     images = mireadimages (filename, slices-1, frames-1, old_matrix);
+elseif (nargin < 6)
+    images = mireadimages (filename, slices-1, frames-1, old_matrix, ...
+                           start_row-1);
+elseif (nargin < 7)
+    images = mireadimages (filename, slices-1, frames-1, old_matrix, ...
+                           start_row-1, num_rows);
 end
