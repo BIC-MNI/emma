f0835bd17d488210ca368d40598770019b924086
wolforth <wolforth>
1994-07-26 11:34:00 +0000

    Changed the link to point to the system directory instead of my personal directory.

--- matlab/general/emmahelp.m
+++ matlab/general/emmahelp.m
@@ -1,7 +1,7 @@
 
 if (strcmp(get(0,'TerminalProtocol'),'x'))
-  !xmosaic http://www.mni.mcgill.ca/users/wolforth/matlab_demos/ &
+  !xmosaic http://www.mni.mcgill.ca/system/mni/matlab/ &
 else
-  disp ('Unable to run XMosaic since this is not an X display.');
+  !lynx http://portia.mni.mcgill.ca/system/mni/matlab/
 end
   
