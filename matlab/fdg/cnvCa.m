b2f8ee27e14248d8001832f242d2e5453d4efd79
wolforth <wolforth>
1995-02-24 17:09:32 +0000

    Replaced with Hiroto's new version...

--- matlab/fdg/cnvCa.m
+++ matlab/fdg/cnvCa.m
@@ -1,22 +1,16 @@
-function cCa = cnvCa(t,Ca,b)
+	function cCa = cnvCa(t,Ca,b)
 
-% To calculate cCa(t) = int_0^t Ca(u)*exp(-b(t-u))du
+% To calculate cCa(t) = int_o^t Ca(u)*exp(-b(t-u))du
 %
-%       cCa = cnvCa(t,Ca,b)
+%	usage: cCa = cnvCa(t,Ca,b)
 %
 % t,Ca,b are column vectors.
-%
-% It is recommended to interpolate Ca(t) with dt <= 0.1 min.
-% When Ca is the integral of a function (Ca'), cCa (output of this code) is
-% equal to the integral of convolution of Ca', if finely sampled.
+% It is recommanded to interpolate Ca(t) with dt <= 0.1 min.
+% When Ca is integral of a function (Ca'), cCa (output of this code)
+% is equal to integral of convolution of Ca', if finely sampled.
 
-Lb=length(b);
-Lt=length(t);
-q=tril(ones(Lt));
-
-u=Ca*ones(1,Lb).*exp(t*b');
-u2=(u+shift_1(u));
-dt=(t-shift_1(t))/2;
-u3=u2.*(dt*ones(1,Lb));
-ui=q*u3;
-cCa=ui.*exp(-t*b');
+% Ca(:,ones(Lb,1))=Ca*ones(1,Lb) but faster.
+t=t(:); b=b(:);
+  Lb=length(b); Lt=length(t); q=tril(ones(Lt));
+  u=Ca(:,ones(Lb,1)).*exp(t*b'); dt=(t-shift_1(t))/2; 
+  cCa=(q*((u+shift_1(u)).*dt(:,ones(Lb,1)))).*exp(-t*b');
