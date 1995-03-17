2a9fe9c32b8abc3026cc98ba0aeb3326dc0f3971
greg <greg>
1995-03-17 00:00:04 +0000

    Nuked it.  calpix is now obsolete, and just a shell for pixelindex.

--- matlab/general/calpix.m
+++ matlab/general/calpix.m
@@ -1,13 +1,4 @@
 function cp = calpix(x,y)
-% CALPIX  generates the vector index of a point
-%
-%       cp = calpix(x,y)
-%
-% generates the vector index (1 to 16384) of 
-% the x and y coordinates of a pixel in a 128*128 image.
-% It uses the simple formula 
-%
-%       cp = round(x) + 128 * round(y-1)
-%
+% CALPIX  Obsolete function - please use PIXELINDEX instead
 
-cp = round(x) + 128 * round(y-1);
+cp = pixelindex ([128 128], x, y);
