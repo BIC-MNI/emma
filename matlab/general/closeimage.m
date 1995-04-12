369e1f5032db9bc31237b061924d8f4244008dbd
greg <greg>
1995-04-12 19:06:54 +0000

    Added use of new global Flags, which is checked to see if we need   to delete a temporary uncompressed copy of a compressed file.

--- matlab/general/closeimage.m
+++ matlab/general/closeimage.m
@@ -7,6 +7,16 @@ function closeimage (handles)
 % Closes one or more image data sets.
 
 for handle = handles
+   eval(['global Flags' int2str(handle)]);
+   eval(['global Filename' int2str(handle)]);
+   eval(['Flags = Flags' int2str(handle) ';']);
+   eval(['Filename = Filename' int2str(handle) ';']);
+   
+   if (Flags(2)) 				  % compressed file?
+      delete (Filename);
+   end
+   
+   eval(['clear global Flags'        int2str(handle)]);
    eval(['clear global Filename'     int2str(handle)]);
    eval(['clear global DimSizes',    int2str(handle)]);
    eval(['clear global FrameTimes'   int2str(handle)]);
