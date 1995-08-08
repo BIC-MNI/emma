0538ee47f95e865e255f171a583e35ff07462a46
greg <greg>
1995-08-08 18:42:09 +0000

    Added disp ('calpix is obsolete...') message.

--- matlab/general/calpix.m
+++ matlab/general/calpix.m
@@ -1,4 +1,5 @@
 function cp = calpix(x,y)
 % CALPIX  Obsolete function - please use PIXELINDEX instead
 
+disp ('calpix is obsolete.  Please use pixelindex instead');
 cp = pixelindex ([128 128], x, y);
