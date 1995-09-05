b0db2cf34f2ac27aabae473ee203ca1cb9e0fa98
greg <greg>
1995-09-05 20:42:33 +0000

    Greatly expanded help to explain both array indexing (0 vs 1) and dimension ordering (voxel vs world).

--- matlab/general/getvoxeltoworld.m
+++ matlab/general/getvoxeltoworld.m
@@ -1,30 +1,92 @@
-function xfm = getvoxel2world (volume)
-% GETVOXEL2WORLD  Build the voxel-to-world coordinate transform for a volume
+function xfm = getvoxeltoworld (volume)
+% GETVOXELTOWORLD  Build the voxel-to-world coordinate transform for a volume
 %
-%   xfm = getvoxel2world (volume)
+%   xfm = getvoxeltoworld (volume)
 % 
-% returns the 4x4 transformation matrix derived from the volume's step
-% sizes, start coordinates, and direction cosines.  See the source for
-% how this is done.
-%
+% returns the 4x4 transformation matrix to convert voxel coordinates to
+% world coordinates.  This matrix assumes that 1) the voxel coordinates
+% are zero-based (C conventions, not MATLAB), and 2) the voxel
+% coordinates are in (x,y,z) order.  If this is not the case, you should
+% be using voxeltoworld and worldtovoxel, as they take care of these
+% issues for you.
+% 
+% The voxel-to-world transformation matrix is derived from the volume's
+% step sizes, start coordinates, and direction cosines.  See the source
+% for how this is done.
+% 
 % The volume argument can be either an image handle or a filename.
-% 
-% NOTE: the transformation matrix can be applied by simple matrix
-% multiplication to a 4x1 column vector of the form [vx vy vz 1]',
-% containing the location of a point in voxel coordinates.  However,
-% if these are slice and pixel numbers obtained within MATLAB, they
-% will be one-based, whereas voxel coordinates are generally
-% zero-based.  (That is, the first slice in the volume is usually
-% called slice zero, whereas by EMMA conventions it is slice one.)
-% Thus, you must first subtract one from each of vx, vy, and vz before
-% applying the matrix returned by getvoxel2world.  Similarly, if you
-% are applying the world-to-voxel transform (the inverse of the matrix
-% returned by getvoxel2world), the resulting coordinates will be
-% zero-based, so you should add one to the resulting x-, y-, and
-% z-coordinates to obtain slice/pixel numbers usable with EMMA.
-% 
+% 						  
+% getvoxeltoworld is a fairly low-level function; usually, you should
+% use voxeltoworld or worldtovoxel, which both call getvoxeltoworld and
+% also handle issues of array indexing and coordinate ordering properly
+% (see below).  These issues are kind of hairy, but quite important;
+% usually, you can let the two higher-level functions take care of them,
+% but if you really want to know the gory details, read on.
+%
+% First, you must consider the base index of arrays.  In C (and thus in
+% most MINC utilities and applications), the first element of an array
+% is element 0.  Thus, voxel coordinates start at (0,0,0); in a volume
+% with all positive step sizes, this will be the inferior, posterior,
+% left-most corner of the volume.  However, the convention in MATLAB is
+% to number arrays starting at 1.  EMMA uses this convention, so (for
+% instance) slice numbers passed to getimages or putimages start at 1,
+% row/column coordinates within an image start at 1, etc.  Thus, one of
+% the steps in translating world coordinates to voxel coordinates *for
+% use within MATLAB* must be to add/subtract 1 to/from voxel
+% coordinates.  If you use the transform matrix returned by
+% getvoxeltoworld, this will *not* be done -- you should use
+% voxeltoworld and/or worldtovoxel.  However, if you're performing the
+% coordinate transformation for use by utilities *outside* of MATLAB,
+% you should stick to the zero-based convention.  This can be done by
+% using the 'external' option with voxeltoworld and worldtovoxel.  (Or,
+% you can just use the matrix returned by getvoxeltoworld -- but beware
+% of dimension ordering!)
+%
+% Next, you must consider dimension ordering.  The canonical order, and
+% the way in which world coordinates are *always* specified, is (x,y,z).
+% However, MINC volumes can be stored in a variety of orders; the most
+% common are transverse (z,y,x), coronal (y,z,x), or sagittal (x,z,y).
+% When coordinates are specified in this order, it's easy to pick out
+% the slice number -- it's just the first coordinate.  Likewise, the
+% "row" coordinate is the second, and the "column" coordinate is the
+% last.  (These are called row and column coordinates because it makes
+% anatomical sense to display volumes with one of the three standard
+% orientations such that the fastest-varying dimension is horizontal on
+% the screen, and the second-fastest-varying dimension is vertical.)
+%
+% Since you always specify coordinates in MATLAB by slice, row, and
+% column (ie. in voxel order), then we obviously need something to take
+% us from world order (x,y,z) to voxel order.  This is the "permutation
+% matrix" P, which is simply a 4x4 identity matrix with the rows
+% reordered according to the volume's dimension ordering.  The
+% `permutation' option to miinquire will give you the permutation matrix
+% to go from voxel order to world order.  (To go the opposite direction,
+% simply invert the permutation matrix.  Actually, since it's an
+% orthogonal matrix, you really only need to transpose it, if you're
+% really worried about shaving off every possible clock cycle.)  
+%
+% Thus, to convert a point from voxel coordinates in voxel order to
+% world coordinates (in world order, of course!), you would use
+% something like this:
+%
+%      T = getvoxeltoworld (volume);
+%      P = miinquire (getimageinfo (volume, 'filename'), 'permutation');
+%      v = [50 30 10 1]';
+%      w = T * P * v;
+%
+% Ignoring the issue of array indexing (here everything must be
+% zero-based, as getvoxeltoworld makes no adjustment for the one-based
+% MATLAB world), this is more or less what the voxeltoworld function
+% does.
+%
+% Note that P is applied first (to reorder the point to world order),
+% and then T is applied -- the transform matrix returned by
+% getvoxeltoworld assumes that the voxel points will be in (x,y,z)
+% order.  You could, of course, post-multiply T by P to get a
+% voxel-to-world matrix that expects voxel coordinates in voxel order.
+%
 % SEE ALSO
-%   voxel2world, world2voxel
+%   voxeltoworld, worldtovoxel
 
 % by Greg Ward 95/3/10
 
