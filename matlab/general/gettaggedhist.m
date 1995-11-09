c46a4153d4bcb31bdad0e20149be4617fdc74289
greg <greg>
1995-11-09 23:16:53 +0000

    Greatly simplified -- now consists of little more than a call to gettaggedregion and some code ripped off from MathWorks' hist function. Also handles any image volume, not just 256x256 transverse.

--- matlab/general/gettaggedhist.m
+++ matlab/general/gettaggedhist.m
@@ -1,52 +1,55 @@
-function [no,xo] = gettaggedhist (handle,tags,bins)
+function [no,xo] = gettaggedhist (handle,tags,bins,progress)
+% GETTAGGEDHIST  creates a histogram of voxel values from a labelled volume
+%
+%     [no,xo] = gettaggedhist (handle, tags [, bins [, progress]])
+%
+% returns vectors no and xo such that bar(xo,no) will plot a histogram
+% of the voxel values in the labelled region given by tags.
+% 
+%     gettaggedhist (handle, tags [, bins [, progress]])
+%     
+% will plot the histogram.
+% 
+% See the help for gettaggedregion for more information on tagged
+% regions.
 
-%
-%
-%        [no,xo] = gettaggedhist (handle, tags [,bins])
-%
-%
-% Assumes transverse orientation
-%
-
-% @COPYRIGHT  :
-%             Copyright 1994 Mark Wolforth, McConnell Brain Imaging Centre,
-%             Montreal Neurological Institute, McGill University.
-%             Permission to use, copy, modify, and distribute this software
-%             and its documentation for any purpose and without fee is
-%             hereby granted, provided that the above copyright notice
-%             appear in all copies.  The author and McGill University make
-%             no representations about the suitability of this software for
-%             any purpose.  It is provided "as is" without express or
-%             implied warranty.
+% @COPYRIGHT :Copyright 1994-95 Mark Wolforth and Greg Ward, McConnell
+%             Brain Imaging Centre, Montreal Neurological Institute,
+%             McGill University.  Permission to use, copy, modify, and
+%             distribute this software and its documentation for any
+%             purpose and without fee is hereby granted, provided that
+%             the above copyright notice appear in all copies.  The
+%             author and McGill University make no representations about
+%             the suitability of this software for any purpose.  It is
+%             provided "as is" without express or implied warranty.
 
 %
 % Check the input arguments
 %
 
-if (nargin < 2)
-  help gettaggedhist
-  error ('Too few input arguments.');
-elseif (nargin == 2)
+err = nargchk (2, 4, nargin);
+if (err)
+   help gettaggedhist
+   error (err);
+end
+
+if (nargin < 3)
   bins = 50;
 end
 
-%
-% Get the image information
-%
+if (nargin < 4)
+  progress = 0;
+end
 
-slices = getimageinfo(handle,'NumSlices');
-
-if (length(bins) == 1)
-  eval(['global Filename',int2str(handle),';']);
-  eval(['filename = Filename',int2str(handle),';']);
-  mins=mireadvar(filename,'image-min');
-  min_val = min(mins);
-  maxs=mireadvar(filename,'image-max');
-  max_val = max(maxs);
+%
+% If caller only supplied number of bins, calculate the bin limits 
+% from the volume min and max.  Otherwise, just use the user-supplied
+% bin list (which is a list of bin midpoints).
+%
   
-  %
-  % Calculate the bin limits
-  %
+if (length(bins) == 1)
+  minmax = getimageinfo (handle, 'MinMax');
+  min_val = minmax(1); max_val = minmax(2);
   
   binwidth = (max_val - min_val) ./ bins;
   xx = min_val + binwidth*[0:bins];
@@ -57,43 +60,11 @@ else
 end
 
 %
-% Initialize no
+% Get the voxel values for the labelled volume, and make a histogram of them
 %
 
-no = zeros (1,length(xo));
-
-%
-% Get the voxel coordinates of the tagged points
-%
-
-[x_v,y_v,z_v] = world2voxel(handle,tags(:,1),tags(:,2),tags(:,3));
-
-if (min(z_v)<1 | max(z_v)>slices)
-  error('Z coordinate is out of range!');
-end
-
-%
-% Get the histograms
-%
-
-disp (['Working on slices ',int2str(min(z_v)),' through ',...
-	int2str(max(z_v))]);
-
-for i=min(z_v):max(z_v)
-
-  disp (['Calculating hist for slice ', int2str(i)]);
-  
-  index = find(z_v==i);
-  yi = y_v(index);
-  xi = x_v(index);
-  mask = zeros(256,256);
-  for j=1:length(xi);
-    mask(xi(j),yi(j)) = 1;
-  end
-  MRI = getimages(handle,i);
-  [n,x] = hist(reshape(mask,length(MRI),1).*MRI,xo);
-  no = no+n;
-end
+values = gettaggedregion (handle, tags, progress);
+no = hist(values,xo);
 
 %
 % If there are no output arguments, then plot
@@ -103,4 +74,3 @@ end
 if (nargout == 0)
   bar (xo,no);
 end
-  
