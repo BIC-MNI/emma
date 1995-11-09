b384dcbf1617ef82f5f01f7288c45cfe0a7daa34
greg <greg>
1995-11-09 16:52:37 +0000

    Changed `x' and `y' to `col' and `row', and beefed up the help.

--- matlab/general/pixelindex.m
+++ matlab/general/pixelindex.m
@@ -1,25 +1,33 @@
-function index = pixelindex (img,x,y)
+function index = pixelindex (img,col,row)
 % PIXELINDEX  generates the vector index of a point
 %
-%       index = pixelindex (img,x,y)
+%       index = pixelindex (img,col,row)
 % 
-% generates the vector index of the x and y coordinates of a pixel in
-% an image.  img is either an image handle or a 2x1 vector describing
-% the size of each image of the form returned by getimageinfo's 
-% `imagesize' option.  That is, the first number is the number of
-% pixels in the image "height" dimension, and the second number is
-% the number of pixels in the "width" dimension.  ("Height" and "width"
-% are respectively the second-fastest and fastest-varying dimensions
-% in the volume.)
+% generates the vector index of the col and row coordinates of a pixel
+% in an image.  col and row are, respectively coordinates along the
+% fastest- and second-fastest varying dimensions of an image volume
+% (the "image dimensions", which are what vary over a single image).
+% 
+% img is either an image handle or a 2x1 vector describing the size of
+% each image of the form returned by getimageinfo's `imagesize'
+% option.  That is, the first number is the number of pixels in the
+% image "height" dimension, and the second number is the number of
+% pixels in the "width" dimension.  ("Height" and "width" are
+% respectively the second-fastest and fastest-varying dimensions in
+% the volume.)
 % 
 % EXAMPLES
 % 
 % If the image is 128x128 pixels, then the vector index will be from
-% 1..16384; pixel (x,y) = (37,24) will translate as follows:
+% 1..16384; pixel (col,row) = (37,24) will translate as follows:
 %
-%     index = width*(y-1) + x = 128*23 + 37 = 2981
+%     index = width*(row-1) + col = 128*23 + 37 = 2981
 % 
 % where width = img(2).
+% 
+% Note that the first pixel in an image has coordinates (1,1), which
+% works out to index 1; the last pixel (in the above example) has
+% coordinates (128,128), which works out to index 128*127+128 = 16384.
 
 % by Greg Ward 95/3/16 (from the obsolete calpix.m)
 
@@ -33,4 +41,4 @@ else
    error ('img must be either an image handle or a 2x1 vector');
 end
 
-index = imgsize(2) * round(y-1) + round(x);
+index = imgsize(2) * round(row-1) + round(col);
