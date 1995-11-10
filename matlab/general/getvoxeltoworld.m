0713233a3443c54a39b8eb107a53165704132875
greg <greg>
1995-11-10 22:05:19 +0000

    Still more revision/expansion of help text. Put a lot of the yucky stuff dealing with fetching steps, starts, and   direction cosines in getimageinfo.  Now getvoxeltoworld doesn't   have to call miinquire at all.

--- matlab/general/getvoxeltoworld.m
+++ matlab/general/getvoxeltoworld.m
@@ -3,26 +3,30 @@ function xfm = getvoxeltoworld (volume)
 %
 %   xfm = getvoxeltoworld (volume)
 % 
-% returns the 4x4 transformation matrix to convert voxel coordinates to
-% world coordinates.  This matrix assumes that 1) the voxel coordinates
-% are zero-based (C conventions, not MATLAB), and 2) the voxel
-% coordinates are in (x,y,z) order.  If this is not the case, you should
-% be using voxeltoworld and worldtovoxel, as they take care of these
-% issues for you.
+% returns the 4x4 transformation matrix to convert voxel coordinates
+% to world coordinates.  This matrix assumes that 1) the voxel
+% coordinates are zero-based (C conventions, not MATLAB), 2) the voxel
+% coordinates are in (x,y,z) order, and 3) the coordinates are
+% relative to unflipped dimensions.  If this is not the case, you
+% should be using voxeltoworld and worldtovoxel, as they take care of
+% these issues for you.
 % 
 % The voxel-to-world transformation matrix is derived from the volume's
 % step sizes, start coordinates, and direction cosines.  See the source
 % for how this is done.
 % 
-% The volume argument can be either an image handle or a filename.
-% 						  
+% The volume argument must be an image handle as returned by openimage
+% or newimage.
+% 
 % getvoxeltoworld is a fairly low-level function; usually, you should
-% use voxeltoworld or worldtovoxel, which both call getvoxeltoworld and
-% also handle issues of array indexing and coordinate ordering properly
-% (see below).  These issues are kind of hairy, but quite important;
-% usually, you can let the two higher-level functions take care of them,
-% but if you really want to know the gory details, read on.
-%
+% use voxeltoworld or worldtovoxel.  These functions both use
+% getvoxeltoworld for their first step, but they also correctly handle
+% issues of array indexing, coordinate ordering, and dimension
+% conversion.  These issues are kind of hairy, but quite important
+% when you wish to do coordinate conversions in MATLAB.  Usually, you
+% can let the two higher-level functions take care of them, but if you
+% really want to know the gory details, read on.
+% 
 % First, you must consider the base index of arrays.  In C (and thus in
 % most MINC utilities and applications), the first element of an array
 % is element 0.  Thus, voxel coordinates start at (0,0,0); in a volume
@@ -38,9 +42,7 @@ function xfm = getvoxeltoworld (volume)
 % voxeltoworld and/or worldtovoxel.  However, if you're performing the
 % coordinate transformation for use by utilities *outside* of MATLAB,
 % you should stick to the zero-based convention.  This can be done by
-% using the 'external' option with voxeltoworld and worldtovoxel.  (Or,
-% you can just use the matrix returned by getvoxeltoworld -- but beware
-% of dimension ordering!)
+% using the 'external' option with voxeltoworld and worldtovoxel.
 %
 % Next, you must consider dimension ordering.  The canonical order, and
 % the way in which world coordinates are *always* specified, is (x,y,z).
@@ -52,27 +54,46 @@ function xfm = getvoxeltoworld (volume)
 % last.  (These are called row and column coordinates because it makes
 % anatomical sense to display volumes with one of the three standard
 % orientations such that the fastest-varying dimension is horizontal on
-% the screen, and the second-fastest-varying dimension is vertical.)
+% the screen, and the second-fastest-varying dimension is vertical.
+% Volumes with non-standard orientations, such as "xyz", will *not* look
+% right when displayed this way... but viewimage does it anyways.)
 %
 % Since you always specify coordinates in MATLAB by slice, row, and
 % column (ie. in voxel order), then we obviously need something to take
 % us from world order (x,y,z) to voxel order.  This is the "permutation
 % matrix" P, which is simply a 4x4 identity matrix with the rows
 % reordered according to the volume's dimension ordering.  The
-% `permutation' option to miinquire will give you the permutation matrix
-% to go from voxel order to world order.  (To go the opposite direction,
-% simply invert the permutation matrix.  Actually, since it's an
-% orthogonal matrix, you really only need to transpose it, if you're
-% really worried about shaving off every possible clock cycle.)  
+% `permutation' option to getimageinfo will give you the permutation
+% matrix to go from voxel order to world order.  (To go the opposite
+% direction, simply invert the permutation matrix.  Actually, since it's
+% an orthogonal matrix, you really only need to transpose it, if you're
+% really worried about shaving off every possible clock cycle.)
 %
-% Thus, to convert a point from voxel coordinates in voxel order to
-% world coordinates (in world order, of course!), you would use
-% something like this:
+% Finally, there is dimension conversion to worry about.  The only form
+% of dimension conversion performed when EMMA reads a MINC file is
+% dimension flipping, so that data always has a positive orientation.
+% Thus, for any flipped dimensions, voxel coordinates have to be
+% subtracted from the maximum voxel index for use with EMMA.  For
+% instance, if a volume's x dimension has 128 elements and a negative
+% step size, then external voxel coordinate 10 corresponds to EMMA voxel
+% coordinate 118 (128 - 10).
+%
+% EXAMPLES
+%
+% To convert a point from voxel coordinates in voxel order to world
+% coordinates in world order, neglecting array indexing and dimension
+% flipping:
 %
 %      T = getvoxeltoworld (volume);
-%      P = miinquire (getimageinfo (volume, 'filename'), 'permutation');
+%      P = getimageinfo (volume, 'permutation');
 %      v = [50 30 10 1]';
 %      w = T * P * v;
+%
+% Here T is the basic voxel-to-world transform, and P reorders points
+% from voxel to world order.  Note the order of application of the two
+% matrices: we first reorder v, and *then* apply T.  This is because T
+% is meant to be applied to points in x,y,z (world) order, so
+% voxel-ordered points *must* be reordered before applying T to them.
 %
 % Ignoring the issue of array indexing (here everything must be
 % zero-based, as getvoxeltoworld makes no adjustment for the one-based
@@ -94,45 +115,21 @@ function xfm = getvoxeltoworld (volume)
 % Get image information - steps, starts, and direction cosines
 %
 
-if (isstr (volume))
-   filename = volume;
-elseif (size(volume) == [1,1])
+if (size(volume) == [1,1])
    eval(['global Filename',int2str(volume),';']);
    eval(['filename = Filename',int2str(volume),';']);
 else
-   error ('volume argument must be either an image handle or a filename');
+   error ('volume argument must be an image handle');
 end
 
-[xstep,ystep,zstep,xstart,ystart,zstart,xdircos,ydircos,zdircos] = ...
-      miinquire (filename, ...
-      'attvalue','xspace','step',...
-      'attvalue','yspace','step',...
-      'attvalue','zspace','step',...
-      'attvalue','xspace','start',...
-      'attvalue','yspace','start',...
-      'attvalue','zspace','start',...
-      'attvalue','xspace','direction_cosines',...
-      'attvalue','yspace','direction_cosines',...
-      'attvalue','zspace','direction_cosines');
+% Get the volume parameters needed for the voxel-to-world transform
 
-if (isempty (xstep) | isempty (ystep) | isempty (zstep))
-   error (['Volume ' filename ' is missing one of xstep, ystep, or zstep']);
-end
+step = diag (getimageinfo (volume, 'Steps'));
+dircos = getimageinfo (volume, 'DirCosines');
+start = getimageinfo (volume, 'Starts');
 
-% Fill in any missing values
+% And construct the transform (no explanation offered because
+% I don't really understand this).
 
-if (isempty (xstart)), xstart = 0; end;
-if (isempty (ystart)), ystart = 0; end;
-if (isempty (zstart)), zstart = 0; end;
-if (isempty (xdircos)), xdircos = [1 0 0]; end;
-if (isempty (ydircos)), ydircos = [0 1 0]; end;
-if (isempty (zdircos)), zdircos = [0 0 1]; end;
-
-% And construct the transformation matrix
-
-xfm = [xstep*xdircos' ...         % first column
-       ystep*ydircos' ...         % second column
-       zstep*zdircos' ...         % third column
-       (xstart*xdircos' + ystart*ydircos' + zstart*zdircos') ];
-
+xfm = [dircos * step, dircos * start];
 xfm = [xfm; 0 0 0 1];
