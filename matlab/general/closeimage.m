cdc5fe564e6c16d79fb7e400dd201b48ea5bdff0
greg <greg>
1996-03-15 22:10:13 +0000

    Changed deletion of temporary (uncompressed) file to also delete the   temp directory; mentioned the deleting of temp files in the help.

--- matlab/general/closeimage.m
+++ matlab/general/closeimage.m
@@ -3,8 +3,11 @@ function closeimage (handles)
 % CLOSEIMAGE  closes image data set(s)
 %
 %     closeimage (handles)
-%
-% Closes one or more image data sets.
+% 
+% Closes one or more image data sets.  If the associated MINC was a
+% compressed file (and therefore uncompressed by openimage), then the
+% temporary file and directory used for the uncompressed data are
+% deleted.
 
 for handle = handles
    eval(['global Flags' int2str(handle)]);
@@ -12,10 +15,15 @@ for handle = handles
    eval(['Flags = Flags' int2str(handle) ';']);
    eval(['Filename = Filename' int2str(handle) ';']);
    
-   if (size(Flags) == [1 2])        % make sure it was actually an open image
-      if (Flags(2)) 				  % compressed file?
+   if (size(Flags) == [1 2])		% make sure it was actually an open
+      if (Flags(2))			% compressed file?
 	 delete (Filename);
+	 slashes = find (Filename == '/');
+	 lastslash = slashes (length (slashes));
+	 delete (Filename(1:(lastslash-1)));
       end
+   else
+      fprintf (2, 'closeimage: warning: invalid image handle (%d)\n', handle);
    end
 
    eval(['clear global Flags'        int2str(handle)]);
