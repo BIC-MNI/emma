024a0392f8c67f1a0701e73a745417ec0fa46990
wolforth <wolforth>
1997-08-14 16:25:10 +0000

    Mark's changes for MATLAB 5 (from orca).

--- matlab/general/tempfilename.m
+++ matlab/general/tempfilename.m
@@ -7,15 +7,20 @@ function fname = tempfilename
 % Requires that a directory /tmp/ exists on the current machine.
 
 now = clock;
-filename = ['/tmp/matimage', int2str(now), '.dat'];
+filename = sprintf ('/tmp/matimage%d%d%d%d%d%s.dat', now(1:5),int2str(now(6)));
 file_handle = fopen (filename,'r');
 
-while (file_handle ~= -1)			% loop until we fail to open the file, ie.
-											% we find one that *doesn't* exist
-   if (file_handle ~= -1)			% if file was successfully opened, close it
-      fclose (file_handle);		% since we don't really want it open
-		filename = ['/tmp/matimage', int2str(now) int2str(1e6*rand) '.dat'];
-		file_handle = fopen (filename, 'r');
+% loop until we fail to open the file, ie.
+% we find one that *doesn't* exist
+
+while (file_handle ~= -1)
+   if (file_handle ~= -1)
+
+	% if file was successfully opened, close it
+
+      	fclose (file_handle);
+	filename = sprintf ('/tmp/matimage%d%d%d%d%d%s%s.dat', now(1:5),int2str(now(6)), int2str(1e6*rand));
+	file_handle = fopen (filename, 'r');
    end
 end
 fname = filename;
